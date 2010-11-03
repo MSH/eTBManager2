d414eb1a9f37eecef03a88c49f6b2f29db91779c
usrivastava <usrivastava@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2010-11-03 21:00:51 +0000

    *Kenya [ENHANCEMENT] Case Managment - Case Data - added new nationalities for kenya
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@93 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

--- src/model/org/msh/mdrtb/entities/enums/Nationality.java
+++ src/model/org/msh/mdrtb/entities/enums/Nationality.java
@@ -3,7 +3,18 @@ package org.msh.mdrtb.entities.enums;
 public enum Nationality {
 
 	NATIVE,
-	FOREIGN;
+	FOREIGN,
+	
+	//Kenya Workspace
+	KENYA,
+	BURUNDI,
+	ETHIOPIA,
+	RWANDA,
+	SOMALIA,
+	SUDAN,
+	TANZANIA,
+	UGANDA,
+	OTHER;
 	
 	public String getKey() {
 		return getClass().getSimpleName().concat("." + name());
