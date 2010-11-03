76449f50fed422302480ebad24bb442376ff5ee6
usrivastava <usrivastava@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2010-11-03 20:52:23 +0000

    *Kenya [BUG] Case Managment - Exam HIV - modified the get exam method to reflect the entity name
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@88 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

--- src/ke/org/msh/tb/ke/ExamHIVHome.java
+++ src/ke/org/msh/tb/ke/ExamHIVHome.java
@@ -3,8 +3,8 @@ package org.msh.tb.ke;
 
 import org.jboss.seam.annotations.Factory;
 import org.jboss.seam.annotations.Name;
+import org.msh.mdrtb.entities.ExamHIV_Ke;
 import org.msh.tb.cases.exams.ExamHome;
-import org.msh.tb.ke.entities.ExamHIV_Ke;
 import org.msh.tb.log.LogInfo;
 
 @Name("examHIVHomeKe")
@@ -13,7 +13,7 @@ public class ExamHIVHome extends ExamHome<ExamHIV_Ke> {
 	private static final long serialVersionUID = 5431512237255765820L;
 
 	@Factory("examHIVKe")
-	public ExamHIV_Ke getExamHIV() {
+	public ExamHIV_Ke getExamHIV_Ke() {
 		return getInstance();
 	}
 	
