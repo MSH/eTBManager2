0e5a1bff5d460c083bde7605979300712bb41f07
usrivastava <usrivastava@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2011-01-11 17:37:12 +0000

    * Bangladesh [Improvement] Changed the package from org.msh.tb.cases.exams to org.msh.tb.bd.cases.exams
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@154 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

---
+++ src/bd/org/msh/tb/bd/cases/exams/ExamBiopsyHome.java
@@ -0,0 +1,31 @@
+package org.msh.tb.bd.cases.exams;
+
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Factory;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.msh.tb.cases.exams.LaboratoryExamHome;
+import org.msh.tb.log.LogInfo;
+
+	@Name("examBiopsyHome")
+	@LogInfo(roleName="EXAM_BIOPSY")
+	@Scope(ScopeType.CONVERSATION)
+	public class ExamBiopsyHome extends LaboratoryExamHome<ExamBiopsy> {
+
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 3613827878816022682L;
+
+		@Factory("examBiopsy")
+		public ExamBiopsy getExamBiopsy() {
+			return getInstance();
+		}
+		
+		@Override
+		public String getJoinFetchHQL() {
+			return super.getJoinFetchHQL() + " left join fetch exam.method met ";
+		}
+	}
+
---
+++ src/bd/org/msh/tb/bd/cases/exams/ExamSkinHome.java
@@ -0,0 +1,31 @@
+package org.msh.tb.bd.cases.exams;
+
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Factory;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.msh.tb.cases.exams.LaboratoryExamHome;
+import org.msh.tb.log.LogInfo;
+
+	@Name("examSkinHome")
+	@LogInfo(roleName="EXAM_SKIN")
+	@Scope(ScopeType.CONVERSATION)
+	public class ExamSkinHome extends LaboratoryExamHome<ExamSkin> {
+
+
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = 3613827878816022682L;
+
+		@Factory("examSkin")
+		public ExamSkin getExamBiopsy() {
+			return getInstance();
+		}
+		
+		@Override
+		public String getJoinFetchHQL() {
+			return super.getJoinFetchHQL() + " left join fetch exam.method met ";
+		}
+	}
+
