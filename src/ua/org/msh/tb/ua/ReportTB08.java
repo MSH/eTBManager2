71b3e9205eb25baa1895befdf9b9e6642b739172
rmemoria <rmemoria@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2011-01-11 14:11:08 +0000

    * Ukraine [Improvements] Reports - TB08 - Adjustments in the TB8 Ukrainian Report;
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@148 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

--- src/ua/org/msh/tb/ua/ReportTB08.java
+++ src/ua/org/msh/tb/ua/ReportTB08.java
@@ -7,7 +7,6 @@ import org.jboss.seam.annotations.Name;
 import org.msh.mdrtb.entities.enums.CaseState;
 import org.msh.mdrtb.entities.enums.ExtraOutcomeInfo;
 import org.msh.mdrtb.entities.enums.InfectionSite;
-import org.msh.mdrtb.entities.enums.MicroscopyResult;
 import org.msh.mdrtb.entities.enums.PatientType;
 import org.msh.tb.indicators.core.Indicator2D;
 import org.msh.tb.indicators.core.IndicatorCultureResult;
@@ -21,13 +20,14 @@ public class ReportTB08 extends Indicator2D {
 
 	private IndicatorTable table2000;
 
-	private static final String statesIn = "(" + CaseState.CURED.ordinal() + "," + 
+	private static final String statesIn = "(" + 
+		CaseState.CURED.ordinal() + "," + 
 		CaseState.TREATMENT_COMPLETED.ordinal() + "," +
 		CaseState.DIED.ordinal() + "," +
 		CaseState.FAILED.ordinal() + "," +
-		CaseState.DEFAULTED.ordinal() + "," +
-		CaseState.TRANSFERRED_OUT.ordinal() + "," +
-		CaseState.DIAGNOSTIC_CHANGED.ordinal() + ")";
+		CaseState.TREATMENT_INTERRUPTION.ordinal() + "," +
+		CaseState.NOT_CONFIRMED.ordinal() + "," +
+		CaseState.TRANSFERRED_OUT.ordinal() + ")";
 
 
 	/* (non-Javadoc)
@@ -56,9 +56,10 @@ public class ReportTB08 extends Indicator2D {
 		table.addColumn(getMessage("uk_UA.ExtraOutcomeInfo.CULTURE_SMEAR"), ExtraOutcomeInfo.CULTURE_SMEAR);
 		table.addColumn(getMessage("uk_UA.ExtraOutcomeInfo.CLINICAL_EXAM"), ExtraOutcomeInfo.CLINICAL_EXAM);
 		table.addColumn(getMessage("uk_UA.ExtraOutcomeInfo.TRANSFER_CATIV"), ExtraOutcomeInfo.TRANSFER_CATIV);
-		table.addColumn(getMessage("CaseState.DEFAULTED"), CaseState.DEFAULTED);
+		table.addColumn(getMessage("uk_UA.ExtraOutcomeInfo.OTHER_CAUSES"), CaseState.FAILED);
+		table.addColumn(getMessage("CaseState.TREATMENT_INTERRUPTION"), CaseState.TREATMENT_INTERRUPTION);
+		table.addColumn(getMessage("CaseState.NOT_CONFIRMED"), CaseState.NOT_CONFIRMED);
 		table.addColumn(getMessage("CaseState.TRANSFERRED_OUT"), CaseState.TRANSFERRED_OUT);
-		table.addColumn(getMessage("CaseState.DIAGNOSTIC_CHANGED"), CaseState.DIAGNOSTIC_CHANGED);
 		table.addColumn(getMessage("global.total"), "TOTAL");
 		
 		String txtPositive = getMessage("global.positive");
@@ -88,10 +89,13 @@ public class ReportTB08 extends Indicator2D {
 				"c.patientType != null and c.state in " + statesIn);
 		addTableValues(table, lst, "_positive");
 
-		filters.setMicroscopyResult(IndicatorMicroscopyResult.NEGATIVE);
+		filters.setMicroscopyResult(IndicatorMicroscopyResult.POSITIVE);
+		String cond = getHQLMicroscopyCondition();
+		filters.setMicroscopyResult(null);
 		filters.setInfectionSite(InfectionSite.PULMONARY);
 		lst = generateValuesByField("c.patientType, c.state, ua.extraOutcomeInfo", 
-				"c.patientType != null and c.state in " + statesIn);
+				"c.patientType != null and c.state in " + statesIn +
+				" and not " + cond);
 		addTableValues(table, lst, "_negative");
 	}
 
@@ -103,11 +107,25 @@ public class ReportTB08 extends Indicator2D {
 		IndicatorFilters filters = getIndicatorFilters();
 		IndicatorTable table = getTable2000();
 
+		// pulmonary positives
 		filters.setInfectionSite(InfectionSite.PULMONARY);
 		List<Object[]> lst = generateValuesByField("c.patientType, c.state, ua.extraOutcomeInfo", 
 				"c.patientType != null and c.state in " + statesIn + " and (" +
-				getHQLMicroscopyOrCultureCondition(IndicatorMicroscopyResult.POSITIVE, IndicatorCultureResult.POSITIVE));
+				getHQLMicroscopyOrCultureCondition(IndicatorMicroscopyResult.POSITIVE, IndicatorCultureResult.POSITIVE) +")");
 		addTableValues(table, lst, "_positive");
+
+		// pulmonary negatives
+		filters.setMicroscopyResult(IndicatorMicroscopyResult.POSITIVE);
+		filters.setCultureResult(IndicatorCultureResult.POSITIVE);
+		String condition = "c.patientType != null " +
+			" and c.state in " + statesIn +
+			" and not " + getHQLMicroscopyCondition() +
+			" and not " + getHQLCultureCondition();
+		filters.setMicroscopyResult(null);
+		filters.setCultureResult(null);
+		filters.setInfectionSite(InfectionSite.PULMONARY);
+		lst = generateValuesByField("c.patientType, c.state, ua.extraOutcomeInfo", condition);
+		addTableValues(table, lst, "_negative");
 	}
 
 
@@ -131,6 +149,12 @@ public class ReportTB08 extends Indicator2D {
 	
 	
 
+	/**
+	 * Add values from the result list to the table
+	 * @param table
+	 * @param lst
+	 * @param micResult
+	 */
 	protected void addTableValues(IndicatorTable table, List<Object[]> lst, String micResult) {
 		for (Object[] vals: lst) {
 			PatientType patientType = (PatientType)vals[0];
@@ -152,25 +176,26 @@ public class ReportTB08 extends Indicator2D {
 			if (state == CaseState.DIED) {
 				if ((extraOutcome == ExtraOutcomeInfo.TB) || (extraOutcome == ExtraOutcomeInfo.OTHER_CAUSES))
 					col = extraOutcome;
-				else col = null;
+				else col = ExtraOutcomeInfo.OTHER_CAUSES; // default value
 			}
 			else if (state == CaseState.FAILED) {
 				if ((extraOutcome == ExtraOutcomeInfo.CLINICAL_EXAM) ||
 					(extraOutcome == ExtraOutcomeInfo.CULTURE) ||
 					(extraOutcome == ExtraOutcomeInfo.TRANSFER_CATIV))
 					 col = extraOutcome;
-				else col = null;
+				else col = state;
 			}
 
 			Float val = ((Long)vals[3]).floatValue();
 
 			if (col != null) {
-				addIdValue(col, key + micResult, val);
-				addIdValue("TOTAL", key + micResult, val);
+				table.addIdValue(col, key + micResult, val);
+				table.addIdValue("TOTAL", key + micResult, val);
 				System.out.println(col + " = " + val);
 			}
 		}
 	}
+
 
 
 	/**
@@ -182,7 +207,8 @@ public class ReportTB08 extends Indicator2D {
 		super.createTable();
 	}
 
-	
+
+
 	/**
 	 * Return table 1000 report, from the original table object from {@link IndicatorTable}
 	 * @return
