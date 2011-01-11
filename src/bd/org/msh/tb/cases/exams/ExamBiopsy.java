7d242e0c0d3173e57ea42510152924b56c9dff83
usrivastava <usrivastava@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2011-01-11 15:03:54 +0000

    * Bangladesh [Improvement] Changed the package from org.msh.tb.cases.exams to org.msh.tb.bd.cases.exams
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@151 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

---
+++ src/bd/org/msh/tb/cases/exams/ExamBiopsy.java
@@ -0,0 +1,29 @@
+package org.msh.tb.bd.cases.exams;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+import org.msh.mdrtb.entities.LaboratoryExamResult;
+import org.msh.tb.bd.entities.enums.BiopsyResult;
+
+@Entity
+public class ExamBiopsy extends LaboratoryExamResult implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1704814081123568208L;
+
+	private BiopsyResult result;
+
+	public BiopsyResult getResult() {
+		return result;
+	}
+
+	public void setResult(BiopsyResult result) {
+		this.result = result;
+	}
+
+
+}
