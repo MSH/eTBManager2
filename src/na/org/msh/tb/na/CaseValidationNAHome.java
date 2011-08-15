3e8938dc537d59c982bcb412342d6501d90da2b1
usrivastava <usrivastava@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2011-08-15 20:04:58 +0000

    * Namibia [BUGFIX] Fix for the advance  search function not working.
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@401 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

--- src/na/org/msh/tb/na/CaseValidationNAHome.java
+++ src/na/org/msh/tb/na/CaseValidationNAHome.java
@@ -1,11 +1,5 @@
 package org.msh.tb.na;
 
-import java.util.Date;
-import java.util.List;
-
-import javax.persistence.EntityManager;
-
-import org.jboss.seam.Component;
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
@@ -13,17 +7,10 @@ import org.jboss.seam.annotations.Scope;
 import org.jboss.seam.annotations.Transactional;
 import org.jboss.seam.annotations.security.Restrict;
 import org.jboss.seam.core.Events;
-import org.msh.tb.cases.CaseHome;
 import org.msh.tb.cases.CaseValidationHome;
-import org.msh.tb.entities.CaseIssue;
 import org.msh.tb.entities.Patient;
 import org.msh.tb.entities.TbCase;
-import org.msh.tb.entities.Tbunit;
-import org.msh.tb.entities.User;
-import org.msh.tb.entities.UserLogin;
-import org.msh.tb.entities.UserWorkspace;
 import org.msh.tb.entities.enums.ValidationState;
-import org.msh.tb.misc.SequenceGenerator;
 
 /**
  * Responsible for the validation process in the case, including pending issues and answers
@@ -36,12 +23,11 @@ public class CaseValidationNAHome extends CaseValidationHome {
 
 	@In(create=true, required=true)
 	private TBUnitCaseNumber tbunitcasenumber;
-	
-	
+
 	@Override
+	@Transactional
+	@Restrict("#{caseHome.canValidate}")
 	public String validate() {
-		// TODO Auto-generated method stub
-		
 		TbCase tbcase = caseHome.getInstance();
 		ValidationState vstate = tbcase.getValidationState();
 		if ((vstate != ValidationState.WAITING_VALIDATION) && (vstate != ValidationState.PENDING_ANSWERED))
@@ -66,7 +52,7 @@ public class CaseValidationNAHome extends CaseValidationHome {
 		else caseNum++;
 		
 		tbcase.setCaseNumber(caseNum);
-		
+				
 		// update case numbers with notification date after this case
 		entityManager.createQuery("update TbCase c " +
 				"set c.caseNumber = c.caseNumber + 1 " + 
@@ -78,12 +64,11 @@ public class CaseValidationNAHome extends CaseValidationHome {
 		entityManager.persist(tbcase);
 		entityManager.flush();
 
-		Events.instance().raiseEvent("case.validate");
+		tbunitcasenumber.generateTBUnitCaseNumber();
 		
-		if(tbunitcasenumber.gernerateTBUnitCaseNumber().equals("error"))
-				return "error";
-		else	
-			return "validated";
+		Events.instance().raiseEvent("case.validate");
+		return "validated";
+		
 	}
 
 
