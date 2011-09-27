8eeab8a6654c78d260bb56788f2ef93bd1866a47
vani <vani@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2011-09-27 19:44:07 +0000

    VR [ customization & Fix] 1. Override persist() 2. set ART and CPT to null before persist in the event of editing HIV result from positive to negative
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@492 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

--- src/na/org/msh/tb/na/ExamHIVHome.java
+++ src/na/org/msh/tb/na/ExamHIVHome.java
@@ -1,9 +1,14 @@
 package org.msh.tb.na;
 
 
+import javax.persistence.EntityManager;
+
 import org.jboss.seam.annotations.Factory;
+import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
+import org.msh.tb.cases.CaseHome;
 import org.msh.tb.cases.exams.ExamHome;
+import org.msh.tb.entities.enums.HIVResult;
 import org.msh.tb.log.LogInfo;
 import org.msh.tb.na.entities.ExamHIV_NA;
 
@@ -11,10 +16,33 @@ import org.msh.tb.na.entities.ExamHIV_NA;
 @LogInfo(roleName="EXAM_HIV")
 public class ExamHIVHome extends ExamHome<ExamHIV_NA> {
 	private static final long serialVersionUID = 5431512237255765820L;
+	@In(create=true)
+	private EntityManager entityManager;
+	@In(required=true) CaseHome caseHome;
+	
 
 	@Factory("examHIVNA")
 	public ExamHIV_NA getExamHIV_NA() {
 		return getInstance();
+	}
+	
+	@Override
+	public String persist() {
+		// TODO Auto-generated method stub
+		ExamHIV_NA examHivNa = getInstance();
+		
+		examHivNa.setTbcase(caseHome.getInstance());
+		if(examHivNa.getResult().toString()=="NEGATIVE"){
+			examHivNa.setCd4Count(null);
+			examHivNa.setCd4StDate(null);
+			examHivNa.setARTstarted(false);
+			examHivNa.setStartedARTdate(null);
+			examHivNa.setArtRegimen(null);
+			examHivNa.setCPTstarted(false);
+			examHivNa.setStartedCPTdate(null);	
+		}
+		entityManager.persist(examHivNa);
+		return "persisted";
 	}
 	
 }
