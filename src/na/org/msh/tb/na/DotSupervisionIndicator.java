f8cf299f86e112e31b2a6a940e7d2ac35a56d1c5
vani <vani@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2011-09-27 20:06:50 +0000

    VR:[Namibia, customization] - DOT Supervision Report 1. Changed from extending Indicator2D to Indicator 2. Few CaseHQLBase methods were overriden
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@496 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

--- src/na/org/msh/tb/na/DotSupervisionIndicator.java
+++ src/na/org/msh/tb/na/DotSupervisionIndicator.java
@@ -5,26 +5,23 @@ import java.util.Calendar;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
-
 import javax.persistence.EntityManager;
-
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.msh.tb.entities.TbCase;
 import org.msh.tb.entities.Tbunit;
 import org.msh.tb.entities.enums.CaseState;
-import org.msh.tb.indicators.core.Indicator2D;
+import org.msh.tb.indicators.core.Indicator;
 import org.msh.tb.indicators.core.IndicatorFilters;
 import org.msh.tb.indicators.core.IndicatorTable;
-import org.msh.tb.indicators.core.IndicatorTable.TableColumn;
 import org.msh.tb.na.TreatmentCalendarHome.PhaseInfo;
 import org.msh.tb.na.entities.CaseDispensingNA;
 import org.msh.utils.date.Period;
 
 @Name("dotSupervisionIndicator")
-public class DotSupervisionIndicator extends Indicator2D{
+
+public class DotSupervisionIndicator extends Indicator{
 
 	/**
 	 * 
@@ -33,8 +30,9 @@ public class DotSupervisionIndicator extends Indicator2D{
 	
 	@In(create=true) EntityManager entityManager;
 	
-
+	private String groupFields;
 	private List<CaseDispensingNA> dispensingList;
+	private List<CaseDispensingNA> dOTdispensingListG;
 	private static final String successRateID = "SRID";  
 	private static final String prescID = "PID";  
 	private static final String dispID = "DID";  
@@ -42,41 +40,42 @@ public class DotSupervisionIndicator extends Indicator2D{
 	@Override
 	protected void createIndicators() {
 		IndicatorFilters filters = getIndicatorFilters();
+		List<CaseDispensingNA> dOTDispensingList =getDOTDispensingList("d.id","exists(select hu.id from TreatmentHealthUnit hu where hu.tbcase.id = d.tbcase.id)");
+		List<CaseDispensingNA> finaldOTDispensingList = getDispensingListPeriod(filters.getIniMonth(), filters.getIniYear(), filters.getTbunitselection().getTbunit(),dOTDispensingList);
 		
-		List<CaseDispensingNA> dispensingList = getDispensingList(filters.getIniMonth(), filters.getIniYear(), filters.getTbunitselection().getTbunit());
-		
-		String msg = getMessage("manag.ind.successrate");
 		IndicatorTable table = getTable();
-		table.addColumn(getMessage("cases.treat.presc"), prescID);
-		table.addColumn(getMessage("cases.treat.disp"), dispID);
-		TableColumn col = table.addColumn("DOT Completion Rate", successRateID);
-//		col.setNumberPattern("#,###,##0");
-		col.setHighlight(true);
-		col.setRowTotal(false);
-		
+			table.addColumn(getMessage("cases.treat.presc"), prescID);
+			table.addColumn(getMessage("cases.treat.disp.taken"), prescID);
+			table.addColumn(getMessage("cases.treat.disp.nottaken"), prescID);
+			table.addColumn(getMessage("cases.treat.disp.selfadmin"), prescID);
+			table.addColumn(getMessage("cases.treat.disp.dotCompletionRate"), prescID);
+
+		CaseDispensingNA caseDispensingNA = new CaseDispensingNA();
 		HashMap<String, Float> dispInfo;
-		Float disp;
+		Float dispTaken;
+		Float dispNotTaken;
+		Float dispNotSup;
 		Float presc = null;
 		Float percentage = 0F;
-		for (Iterator<CaseDispensingNA> iterator = dispensingList.iterator(); iterator.hasNext();) {
+		for(int i =0; i < finaldOTDispensingList.size(); i++){
 			presc = null;
-			CaseDispensingNA caseDispensingNA = (CaseDispensingNA) iterator.next();
+			caseDispensingNA = entityManager.find(CaseDispensingNA.class, finaldOTDispensingList.get(i).getId());
 			dispInfo = getPrescribedDays(caseDispensingNA.getTbcase());
-			disp = (caseDispensingNA.getDispensingDays()!=null?new Float(caseDispensingNA.getDispensingDays().getNumDispensingDays()):null);
+			dispTaken = (caseDispensingNA.getDispensingDays()!=null?new Float(caseDispensingNA.getDispensingDays().getNumDispensingDaysTaken()):null);
+			dispNotTaken = (caseDispensingNA.getDispensingDays()!=null?new Float(caseDispensingNA.getDispensingDays().getNumDispensingDaysNotTaken()):null);
+			dispNotSup = (caseDispensingNA.getDispensingDays()!=null?new Float(caseDispensingNA.getDispensingDays().getNumDispensingDaysNotSupervised()):null);
 			presc = (dispInfo.get("TotalPrescribed")!=null?dispInfo.get("TotalPrescribed"):0F);
-			if(disp!=null&&presc!=null)
-				percentage = disp/presc*100;
-			addValue(getMessage("cases.treat.disp"), caseDispensingNA.getTbcase().getPatient().getName(), disp);
-			addValue(getMessage("cases.treat.presc"), caseDispensingNA.getTbcase().getPatient().getName(), presc);
-			addValue(msg, caseDispensingNA.getTbcase().getPatient().getName(), percentage);
+				if(dispTaken!=null&&presc!=null)
+					percentage = (dispTaken + dispNotSup)/presc*100;
+		addValue(getMessage("cases.treat.presc"), caseDispensingNA.getTbcase().getPatient().getName(), presc);
+		addValue(getMessage("cases.treat.disp.taken"), caseDispensingNA.getTbcase().getPatient().getName(), dispTaken);
+		addValue(getMessage("cases.treat.disp.nottaken"), caseDispensingNA.getTbcase().getPatient().getName(), dispNotTaken);
+		addValue(getMessage("cases.treat.disp.selfadmin"), caseDispensingNA.getTbcase().getPatient().getName(), dispNotSup);
+		addValue(getMessage("cases.treat.disp.dotCompletionRate"), caseDispensingNA.getTbcase().getPatient().getName(), percentage);
 		}
 	}
-		 
-	/**
-	 * Return the information about the totaldispensed and total prescribed for the month and year.
-	 * @param tbcase
-	 * @return
-	 */
+	
+
 	private HashMap<String, Float> getPrescribedDays(TbCase tbcase) {
 		TreatmentCalendarHome treatmentCal = new TreatmentCalendarHome(tbcase, entityManager);	
 		List<PhaseInfo> phases = treatmentCal.getPhases();
@@ -118,22 +117,51 @@ public class DotSupervisionIndicator extends Indicator2D{
 		dispensingList = filteredLst;
 		return dispensingList;
 	}
+	/**
+	 * Return the list of cases and its dispensing information of a specific month/year
+	 * @param tbunit
+	 * @param year 
+	 * @param month 
+	 * @return List of {@link CaseDispensingNA} objects
+	 */
+	public List<CaseDispensingNA> getDispensingListPeriod(Integer month, Integer year, Tbunit tbunit, List<CaseDispensingNA> dOTcasedispensingNA) {
+		List<CaseDispensingNA> filteredLst = new ArrayList<CaseDispensingNA>();
+		CaseDispensingNA caseDispensingNA = new CaseDispensingNA();
+		for(int i =0; i < dOTcasedispensingNA.size(); i++){
+			caseDispensingNA = entityManager.find(CaseDispensingNA.class, dOTcasedispensingNA.get(i));
+			Period treatPeriod = caseDispensingNA.getTbcase().getTreatmentPeriod();
+			Calendar c = Calendar.getInstance();
+			c.set(year, month, 1);
+			java.util.Date iniDt = c.getTime();
+			c.set(Calendar.DAY_OF_MONTH, c.getMaximum(Calendar.DAY_OF_MONTH));
+			java.util.Date endDt = c.getTime();
+			Period indicatorPeriod = new Period(iniDt, endDt);
+			if(treatPeriod.contains(indicatorPeriod)||treatPeriod.isIntersected(indicatorPeriod))
+				filteredLst.add(caseDispensingNA);
+		}
+		dispensingList = filteredLst;
+		return dispensingList;
+	}
 
 	/**
 	 * Create the list of {@link CaseDispensingNA} objects
 	 */
 	protected void createCasesList(Integer month, Integer year, Tbunit tbunit) {
-		Integer tbunitId = tbunit.getId();
-		if ((month == null) || (year == null) || (tbunitId == null))
-			return;
+		String tbunitFilter="";
 		
+		if(tbunit!=null)
+			tbunitFilter = "and hu.tbunit.id = " + tbunit.getId() + ") ";
+		else
+			tbunitFilter = "";
+			
+			Integer tbunitId = tbunit.getId();
 		dispensingList = new ArrayList<CaseDispensingNA>();
 		
 		// load the dispensing days information recorded in the database
 		dispensingList = entityManager.createQuery("from CaseDispensingNA d join fetch d.tbcase c " +
 				"join fetch c.patient p " +
 				"where exists(select hu.id from TreatmentHealthUnit hu where hu.tbcase.id = c.id and hu.period.endDate = c.treatmentPeriod.endDate " +
-				"and hu.tbunit.id = " + tbunitId + ") " +
+				tbunitFilter+
 				"and d.month = " + (month + 1) + " and d.year = " + year +
 				" and c.state = " + CaseState.ONTREATMENT.ordinal())
 				.getResultList();
@@ -173,6 +201,21 @@ public class DotSupervisionIndicator extends Indicator2D{
 		}
 	}
 
+
+	/**
+	 * Create the list of {@link CaseDispensingNA} objects
+	 */
+	public List<CaseDispensingNA> getDOTDispensingList(String fields, String condition) {
+		setOutputSelected(false);
+		setGroupFields(fields);
+		setCondition(condition);
+		dispensingList = new ArrayList<CaseDispensingNA>();
+	//	dispensingList = createQuery().getResultList();
+		String hql = createHQL();
+		dispensingList = entityManager.createQuery(hql).getResultList();
+		return dispensingList;
+	}
+	
 	
 	protected CaseDispensingNA findCaseDispensing(TbCase tbcase) {
 		for (CaseDispensingNA info: dispensingList) {
@@ -182,4 +225,36 @@ public class DotSupervisionIndicator extends Indicator2D{
 		}
 		return null;
 	}
+	
+		@Override
+	protected String getHQLSelect() {
+		
+		return "select d.id";
+			
+		
+	}	
+	
+	@Override
+	protected String getHQLFrom() {
+		// TODO Auto-generated method stub
+		return "from CaseDispensingNA d";
+		
+	}
+	
+	@Override
+	protected String getHQLJoin() {
+		String joinStr = "join d.tbcase c ";
+		String s = super.getHQLJoin();
+
+		if (s != null)
+			joinStr = joinStr.concat(s);
+		return joinStr;
+	}
+	
+	@Override
+	protected String getPeriodCondition() {
+		String s = "c.treatmentPeriod.endDate >= #{indicatorFilters.iniDate}";
+		return s;
+	}
+	
 }
