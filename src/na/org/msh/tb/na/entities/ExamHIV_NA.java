e5987fae1e7ca073e21c57382268740210a5e4b0
vani <vani@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2011-09-27 19:49:15 +0000

    VR [Namibia, customization] : ExamHIV_NA is made to extend ExamHiv
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@493 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

--- src/na/org/msh/tb/na/entities/ExamHIV_NA.java
+++ src/na/org/msh/tb/na/entities/ExamHIV_NA.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.util.Date;
 
 import javax.persistence.Column;
+import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
@@ -13,6 +14,7 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 import org.msh.tb.entities.CaseData;
+import org.msh.tb.entities.ExamHIV;
 import org.msh.tb.entities.FieldValue;
 import org.msh.tb.entities.enums.HIVResult;
 import org.msh.tb.entities.enums.HIVResultKe;
@@ -24,33 +26,18 @@ import org.msh.tb.log.FieldLog;
  * Records information about an HIV result during the treatment
  */
 @Entity
+@DiscriminatorValue("na")
 @Table(name="examhiv_na")
-public class ExamHIV_NA extends CaseData implements Serializable {
+public class ExamHIV_NA extends ExamHIV {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 8053860831668724183L;
 
-	private HIVResult result;
-	
 	@Temporal(TemporalType.DATE)
 	private Date resultDate;
 	
-	@Temporal(TemporalType.DATE)
-	private Date startedARTdate;
-	
-	@Temporal(TemporalType.DATE)
-	private Date startedCPTdate;
-	
-	@Column(length=100)
-	private String laboratory;
-	
-	private Integer cd4Count;	
-	
-	@Temporal(TemporalType.DATE)
-	private Date cd4StDate;
-
 	private Integer artNumber;
 	
 	@ManyToOne(fetch=FetchType.LAZY)
@@ -58,79 +45,12 @@ public class ExamHIV_NA extends CaseData implements Serializable {
 	@FieldLog(key="TbField.ART_REGIMEN")
 	private FieldValue artRegimen;	
 	
-	
-	public boolean isARTstarted() {
-		return startedARTdate != null;
-	}
-	
-	public boolean isCPTstarted() {
-		return startedCPTdate != null;
-	}
-	
-	public void setCPTstarted(boolean value) {
-		if (!value)
-			startedCPTdate = null;
-	}
-	
-	public void setARTstarted(boolean value) {
-		if (!value)
-			startedARTdate = null;
-	}
-	
-	public String getLaboratory() {
-		return laboratory;
-	}
-
-	public void setLaboratory(String laboratory) {
-		this.laboratory = laboratory;
-	}
-
-	public HIVResult getResult() {
-		return result;
-	}
-
-	public void setResult(HIVResult result) {
-		this.result = result;
-	}
-
 	public Date getResultDate() {
 		return resultDate;
 	}
 
 	public void setResultDate(Date resultDate) {
 		this.resultDate = resultDate;
-	}
-
-	public Date getStartedARTdate() {
-		return startedARTdate;
-	}
-
-	public void setStartedARTdate(Date startedARTdate) {
-		this.startedARTdate = startedARTdate;
-	}
-
-	public Date getStartedCPTdate() {
-		return startedCPTdate;
-	}
-
-	public void setStartedCPTdate(Date startedCPTdate) {
-		this.startedCPTdate = startedCPTdate;
-	}
-	
-	public Integer getCd4Count() {
-		return cd4Count;
-	}
-
-	public void setCd4Count(Integer cd4Count) {
-		this.cd4Count = cd4Count;
-	}
-
-	public Date getCd4StDate() {
-		return cd4StDate;
-	}
-
-	public void setCd4StDate(Date cd4StDate) {
-		this.cd4StDate = cd4StDate;
 	}
 
 	public Integer getArtNumber() {
