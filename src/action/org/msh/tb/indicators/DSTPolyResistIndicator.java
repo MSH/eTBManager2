1382a834e0bbe7ad9ef0357c4bfc7d05f171fa09
vani <vani@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2011-10-18 16:56:10 +0000

    [Generic,Small Fix] - DST Resistance PRofile Report - The result set table columns 'Never Treated%' and 'Previously Treated' were previously reading un-corresponding results. It is now swapped.
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@529 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

--- src/action/org/msh/tb/indicators/DSTPolyResistIndicator.java
+++ src/action/org/msh/tb/indicators/DSTPolyResistIndicator.java
@@ -46,8 +46,8 @@ public class DSTPolyResistIndicator extends Indicator2D {
 		
 		String[] message = new String[5];
 		newp 		= getMessage("manag.ind.dstprofile.nevertreated");
-		newPer 		= getMessage("manag.ind.dstprofile.prevtreated");
-		oldp 		= getMessage("manag.ind.dstprofile.nevertreatedpercent");
+		newPer 		= getMessage("manag.ind.dstprofile.nevertreatedpercent");
+		oldp 		= getMessage("manag.ind.dstprofile.prevtreated");
 		oldPer 		= getMessage("manag.ind.dstprofile.prevtreatedpercent");
 		total 		= getMessage("global.total");
 		medicine 	= "";
@@ -126,11 +126,12 @@ public class DSTPolyResistIndicator extends Indicator2D {
 		
 		IndicatorTable table = getTable();
 		table.addColumn(newp, null);
-		table.addColumn(oldp, null);
+		//table.addColumn(oldp, null);
 
 		TableColumn newPercent = table.addColumn(newPer, null);
 		newPercent.setHighlight(true);
 		
+		table.addColumn(oldp, null);
 		TableColumn oldPercent = table.addColumn(oldPer, null);
 		oldPercent.setHighlight(true);
 		
