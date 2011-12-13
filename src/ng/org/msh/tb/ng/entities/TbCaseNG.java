d5ab66134c8b3dea5707b2e70978c0f3951818eb
vani <vani@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2011-12-13 17:18:20 +0000

    VR[Nigeria] : adding 2 new fields - TB Diagnosis Date and TB Registration Number
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@566 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

--- src/ng/org/msh/tb/ng/entities/TbCaseNG.java
+++ src/ng/org/msh/tb/ng/entities/TbCaseNG.java
@@ -1,12 +1,16 @@
 package org.msh.tb.ng.entities;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 import org.msh.tb.entities.TbCase;
 import org.msh.tb.log.FieldLog;
@@ -23,6 +27,12 @@ public class TbCaseNG extends TbCase{
 	@OneToMany(cascade={CascadeType.MERGE, CascadeType.PERSIST}, mappedBy="tbcase")
 	@FieldLog(ignore=true)
 	private List<CaseDispensing_Ng> dispng = new ArrayList<CaseDispensing_Ng>();
+	
+	@Temporal(TemporalType.DATE)
+	@Column(name="TB_DIAGNOSIS_DATE")
+	private Date tbDiagnosisDate;
+	
+	private Integer tbRegistrationNumber;
 
 	public List<CaseDispensing_Ng> getDispng() {
 		return dispng;
@@ -31,13 +41,20 @@ public class TbCaseNG extends TbCase{
 	public void setDispng(List<CaseDispensing_Ng> dispng) {
 		this.dispng = dispng;
 	}
+
+	public Date getTbDiagnosisDate() {
+		return tbDiagnosisDate;
+	}
 	
-	
+	public void setTbDiagnosisDate(Date tbDiagnosisDate) {
+		this.tbDiagnosisDate = tbDiagnosisDate;
+	}
 
+	public Integer getTbRegistrationNumber() {
+		return tbRegistrationNumber;
+	}
 
-	
-	
-
-
-
+	public void setTbRegistrationNumber(Integer tbRegistrationNumber) {
+		this.tbRegistrationNumber = tbRegistrationNumber;
+	}
 }
