7fab74dafb4b2312a3ef78dc37b0d8311665df3f
vani <vani@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2012-01-03 21:27:40 +0000

    Nigeria[Customize]-Adding new customized MedicalExaminationNGHome for Nigerian Implementation
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@596 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

---
+++ src/ng/org/msh/tb/ng/MedicalExaminationNGHome.java
@@ -0,0 +1,45 @@
+package org.msh.tb.ng;
+
+import org.jboss.seam.annotations.Factory;
+import org.jboss.seam.annotations.Name;
+import org.msh.tb.cases.exams.ExamHome;
+import org.msh.tb.ng.entities.MedicalExamination_Ng;
+import org.msh.tb.ng.entities.enums.HealthFacility;
+;
+/**
+ * Specific code to handle Medical Examination functionalities for Nigerian
+ * workspace
+ * 
+ * @author Vani Rao
+ * 
+ */
+@Name("medicalExaminationNGHome")
+public class MedicalExaminationNGHome extends ExamHome<MedicalExamination_Ng>{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8412334261581230829L;
+	
+	//private HealthFacility healthFacility;
+	
+	@Factory("medicalExamination_Ng")
+	public MedicalExamination_Ng getMedicalExamination() {
+		return getInstance();
+	}
+	
+	@Override
+	public String persist() {
+		return super.persist();
+	}
+
+//	public HealthFacility getHealthFacility() {
+//		return healthFacility;
+//	}
+//	
+//	public void setHealthFacility(HealthFacility healthFacility) {
+//		this.healthFacility = healthFacility;
+//	}
+
+
+}
