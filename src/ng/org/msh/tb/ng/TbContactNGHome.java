34ff7ec2cb6b70ca6d556996a9c6b7e3274509a9
vani <vani@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2012-01-03 21:32:16 +0000

    Nigeria[Improvement/Customization]-Adding new class for Nigerian implementation
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@598 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

---
+++ src/ng/org/msh/tb/ng/TbContactNGHome.java
@@ -0,0 +1,59 @@
+package org.msh.tb.ng;
+
+import org.jboss.seam.annotations.Factory;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+import org.msh.tb.EntityHomeEx;
+import org.msh.tb.TagsCasesHome;
+import org.msh.tb.br.entities.TbContactBR;
+import org.msh.tb.cases.CaseHome;
+import org.msh.tb.entities.TbCase;
+import org.msh.tb.entities.TbContact;
+import org.msh.tb.ng.entities.TbContact_Ng;
+
+/**
+* @author Vani Rao
+*
+* 
+*/
+@Name("tbContactNGHome")
+public class TbContactNGHome extends EntityHomeEx<TbContact_Ng> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1176239729083664259L;
+	
+	@In(required=true) CaseHome caseHome;
+
+	@Factory("tbContactNG")
+	public TbContact_Ng getTbContact() {
+		return getInstance();
+	}
+	
+	@Override
+	public String persist() {		
+		TbContact_Ng con = getInstance();
+		TbCase tbcase = caseHome.getInstance();
+		
+		con.setTbcase(tbcase);
+		tbcase.getContacts().add(con);
+		
+		String s = super.persist();
+		
+		//if ("persisted".equals(s))
+		//	TagsCasesHome.instance().updateTags(tbcase);
+		
+		return s;
+	}
+	
+	@Override
+	public String remove() {
+		TbCase tbcase = caseHome.getInstance();
+		tbcase.getContacts().remove(getInstance());
+		
+		if (super.remove().equals("removed"))
+			 return "contact-removed";
+		else return "error";
+	}
+}
