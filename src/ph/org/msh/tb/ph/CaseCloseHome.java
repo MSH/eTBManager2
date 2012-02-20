703cb49ba1a7f31e670aac48a134939db73093ae
chesa <chesa@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2012-02-20 04:23:03 +0000

    *Philippines[Additions] - Customization for PH caseCloseHome
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@689 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

---
+++ src/ph/org/msh/tb/ph/CaseCloseHome.java
@@ -0,0 +1,156 @@
+package org.msh.tb.ph;
+
+import java.util.Date;
+
+import javax.persistence.EntityManager;
+
+import org.jboss.seam.annotations.AutoCreate;
+import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Transactional;
+import org.jboss.seam.faces.FacesMessages;
+import org.jboss.seam.framework.Controller;
+import org.msh.tb.cases.treatment.TreatmentHome;
+import org.msh.tb.entities.TbCase;
+import org.msh.tb.cases.CaseHome;
+import org.msh.tb.entities.enums.CaseClassification;
+import org.msh.tb.entities.enums.CaseState;
+import org.msh.utils.date.Period;
+
+@Name("caseCloseHomeph")
+@AutoCreate
+public class CaseCloseHome extends Controller{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@In(create=true) CaseHome caseHome;
+	@In(create=true) FacesMessages facesMessages;
+	@In(create=true) TreatmentHome treatmentHome;
+	@In EntityManager entityManager;
+
+	
+	private Date date;
+	private CaseState state;
+	private String comment;
+	
+	private static final CaseState[] outcomesMDR = {
+		CaseState.CURED, 
+		CaseState.DEFAULTED, 
+		CaseState.DIED, 
+		CaseState.FAILED,
+		CaseState.TREATMENT_COMPLETED,
+		CaseState.TRANSFERRED_OUT,
+		CaseState.DIAGNOSTIC_CHANGED_TO_NOT_DRTB,
+		CaseState.DIAGNOSTIC_CHANGED_TO_MDR,
+		CaseState.DIAGNOSTIC_CHANGED_TO_MONO,
+		CaseState.DIAGNOSTIC_CHANGED_TO_POLY,
+		CaseState.DIAGNOSTIC_CHANGED_TO_XDR,
+		CaseState.OTHER};
+
+	private static final CaseState[] outcomesTB = {
+		CaseState.CURED, 
+		CaseState.DEFAULTED, 
+		CaseState.DIED, 
+		CaseState.FAILED,
+		CaseState.TREATMENT_COMPLETED,
+		CaseState.TRANSFERRED_OUT,
+		CaseState.DIAGNOSTIC_CHANGED,
+		CaseState.MDR_CASE,
+		CaseState.OTHER};
+	
+	/**
+	 * Closes an opened case, setting its outcome
+	 * @return
+	 */
+	@Transactional
+	public String closeCase() {
+		TbCase tbcase = caseHome.getInstance();
+
+		Date dt = tbcase.getDiagnosisDate();
+		if ((dt != null) && (date.before(dt))) {
+			facesMessages.addFromResourceBundle("cases.close.msg1");
+			return "error";
+		}
+
+		if ((tbcase.getTreatmentPeriod() != null) && (!tbcase.getTreatmentPeriod().isEmpty()))
+			treatmentHome.cropTreatmentPeriod(new Period(tbcase.getTreatmentPeriod().getIniDate(), date));
+		
+		tbcase.setOutcomeDate(date);
+
+		tbcase.setState(state);
+		if (state.equals(CaseState.OTHER))
+			 tbcase.setOtherOutcome(comment);
+		else tbcase.setOtherOutcome(null);
+
+		// save transaction log
+		caseHome.getLogDetailWriter().addTableRow("TbCase.outcomeDate", date);
+		caseHome.getLogDetailWriter().addTableRow("TbCase.otherOutcome", tbcase.getState().getKey());
+		caseHome.saveExecuteTransaction("CASE_CLOSE");
+		
+		// save case changes
+		caseHome.setTransactionLogActive(false);
+		caseHome.persist();
+		caseHome.updateCaseTags();
+
+		return "case-closed";
+	}
+	
+	@Transactional
+	public String reopenCase() {
+		TbCase tbcase = caseHome.getInstance();
+		
+		if ((tbcase.getTreatmentPeriod() == null) || (tbcase.getTreatmentPeriod().isEmpty()))
+			 tbcase.setState(CaseState.WAITING_TREATMENT);
+		else tbcase.setState(CaseState.ONTREATMENT);
+		
+		tbcase.setOtherOutcome(null);
+
+		caseHome.saveExecuteTransaction("CASE_REOPEN");
+		caseHome.setTransactionLogActive(false);
+		caseHome.persist();
+		caseHome.updateCaseTags();
+		
+		return "case-reopened";
+	}
+
+
+	/**
+	 * Return the available outcomes of a case, according to its classification (TB or DR-TB)
+	 * @return Array of {@link CaseState} enumerations
+	 */
+	public CaseState[] getOutcomes() {
+		if (caseHome.getInstance().getClassification() == CaseClassification.DRTB)
+			 return outcomesMDR;
+		else return outcomesTB;
+	}
+
+
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public CaseState getState() {
+		return state;
+	}
+
+	public void setState(CaseState state) {
+		this.state = state;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	
+}
