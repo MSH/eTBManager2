b4fcd3c2ba38082665f2e1c73031b6d8b4e9b05d
rmemoria <rmemoria@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2012-02-23 17:19:51 +0000

    * Generic [New] Administration - New user transaction statistics report; * Generic [Bug] Medicines - Movements don't move to different pages;
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@696 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

--- src/action/org/msh/tb/medicines/movs/MovementFilters.java
+++ src/action/org/msh/tb/medicines/movs/MovementFilters.java
@@ -5,8 +5,10 @@ import java.util.Date;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpServletRequest;
 
+import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
 import org.msh.tb.entities.enums.MovementType;
 
 /**
@@ -15,6 +17,7 @@ import org.msh.tb.entities.enums.MovementType;
  *
  */
 @Name("movementFilters")
+@Scope(ScopeType.CONVERSATION)
 public class MovementFilters {
 
 	@In(create=true) MovementsQuery movements;
