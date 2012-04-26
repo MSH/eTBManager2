8d2eda8162217e010b4b9acab87aa3d7568607ce
vani <vani@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2012-04-26 14:34:31 +0000

    Generic[Fix]-DST report *% calculation of cases is taken of entire cases with any DST result and not just DST Resistant results as it was earlier
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@1022 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

--- src/action/org/msh/tb/indicators/DSTUniqueResistIndicator.java
+++ src/action/org/msh/tb/indicators/DSTUniqueResistIndicator.java
@@ -45,10 +45,29 @@ public class DSTUniqueResistIndicator extends ResistProfileIndicatorBase {
 
 	@Override
 	public String getSqlQuery() {
+		//VR: removing count(*) in percentage_new as the percentage is to be taken of all DST results irrespective of resistant / Susceptible
+//		String sqlStat = " select s.ABBREV_NAME1, sum(if(c.patientType=0, 1, 0)) as newp, " +
+//		"				(sum(if(c.patientType=0, 1, 0))/count(*)*100) as percentage_new, " +
+//		"				sum(if(c.patientType>0, 1, 0))as other, " +
+//		"				(sum(if(c.patientType>0, 1, 0))/count(*)*100) as percentage_old, " +
+//		"				count(*) as total " +
+//		"			from tbcase c, tbunit u, examdst ed, examdstresult er, substance s " +
+//		"			where c.NOTIFICATION_UNIT_ID = u.id " +
+//		"				and u.WORKSPACE_ID = " +defaultWorkspace.getId()+
+//		"				and ed.CASE_ID = c.id " +
+//		"				and er.EXAM_ID = ed.id " +
+//		"				and er.SUBSTANCE_ID = s.id " +
+//		"				and s.ABBREV_NAME1 in ('H', 'R', 'E', 'S') " +
+//		"				and er.result = " + DstResult.RESISTANT.ordinal()+
+//		"				and ed.dateCollected = (select MAX(aux.dateCollected) from examdst aux where aux.CASE_ID = c.id) " +
+//		"				and (select count(*) from examdstresult auxer where ed.id = auxer.EXAM_ID " +
+//		"				and auxer.result = 1) <= 1 " +
+//		"			group by s.ABBREV_NAME1";
+		
 		String sqlStat = " select s.ABBREV_NAME1, sum(if(c.patientType=0, 1, 0)) as newp, " +
-		"				(sum(if(c.patientType=0, 1, 0))/count(*)*100) as percentage_new, " +
+		"				sum(if(c.patientType=0, 1, 0)) as percentage_new, " +
 		"				sum(if(c.patientType>0, 1, 0))as other, " +
-		"				(sum(if(c.patientType>0, 1, 0))/count(*)*100) as percentage_old, " +
+		"				sum(if(c.patientType>0, 1, 0)) as percentage_old, " +
 		"				count(*) as total " +
 		"			from tbcase c, tbunit u, examdst ed, examdstresult er, substance s " +
 		"			where c.NOTIFICATION_UNIT_ID = u.id " +
