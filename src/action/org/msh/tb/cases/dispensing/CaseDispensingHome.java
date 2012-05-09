640184bb4593b36ceadce3adaae61e594fbdafe1
vani <vani@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2012-05-09 18:46:13 +0000

    Generic[Fix]-Ceases dispensing entry for future dates
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@1113 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

--- src/action/org/msh/tb/cases/dispensing/CaseDispensingHome.java
+++ src/action/org/msh/tb/cases/dispensing/CaseDispensingHome.java
@@ -1,5 +1,9 @@
 package org.msh.tb.cases.dispensing;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -91,6 +95,62 @@ public class CaseDispensingHome {
 		}
 	}
 
+	/**
+	 * Checks if medicine dispensing can be entered. Ceases future date entries.
+	 */
+	public boolean isDispensable(Integer day) {			
+		Date currDate = getCurrentDate();
+		Date dispDate = getDispDate(year,month,day);
+		
+		if(dispDate.before(currDate))
+			return true;
+		if(dispDate.compareTo(currDate)==0)
+			return true;
+		else 
+			return false;		
+	}
+	
+	/**
+	 * fetches current date
+	 */
+	public Date getCurrentDate(){
+		Calendar cal = Calendar.getInstance();
+		String date_format = "yyyyMMdd";
+		SimpleDateFormat sdf = new SimpleDateFormat(date_format);
+		Date date = null;
+		try {
+			 date = (Date)sdf.parse(sdf.format(cal.getTime()));
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return date;
+	}
+	
+	public Date getDispDate(int yr, int mon, int d){
+		String month = null;
+		String day = null;
+		if((mon+1)/10==0){
+			month="0".concat(Integer.toString(mon+1));
+		}
+		else
+			month=Integer.toString(mon+1);
+		if((d/10==0))
+			day="0".concat(Integer.toString(d));
+		else
+			day=Integer.toString(d);
+		String strDate = Integer.toString(yr).concat(month).concat(day);
+		String date_format = "yyyyMMdd";
+		SimpleDateFormat sdf = new SimpleDateFormat(date_format);
+		Date date = null;
+		try {
+			date = (Date)sdf.parse(strDate);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return date;
+	}
 
 	/**
 	 * @return the month
