4bc5b242b65f5f4442d6b95f6b5530e77d620306
okurasov <okurasov@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2012-05-28 18:22:41 +0000

    AZ bug fixed wrong import removed
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@1210 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

--- src/az/org/msh/tb/az/CaseEditingAZHome.java
+++ src/az/org/msh/tb/az/CaseEditingAZHome.java
@@ -1,11 +1,19 @@
 package org.msh.tb.az;
 
+import java.util.List;
+
+import org.jboss.seam.Component;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Transactional;
+import org.jboss.seam.contexts.Contexts;
 import org.msh.tb.cases.CaseEditingHome;
 import org.msh.tb.entities.Address;
+import org.msh.tb.entities.AdministrativeUnit;
 import org.msh.tb.entities.MedicalExamination;
+import org.msh.tb.entities.Patient;
 import org.msh.tb.entities.TbCase;
+import org.msh.tb.entities.Tbunit;
+import org.msh.tb.entities.UserWorkspace;
 import org.msh.tb.entities.enums.CaseState;
 import org.msh.tb.entities.enums.MedAppointmentType;
 import org.msh.tb.entities.enums.ValidationState;
@@ -14,7 +22,7 @@ import org.msh.tb.entities.enums.YesNoType;
 
 @Name("caseEditingAZHome")
 public class CaseEditingAZHome extends CaseEditingHome{
-	
+
 	@Override
 	@Transactional
 	public String saveNew() {
@@ -72,5 +80,85 @@ public class CaseEditingAZHome extends CaseEditingHome{
 		caseHome.updateCaseTags();
 
 		return (regimenType == 2? "individualized": "persisted");
+	}
+	
+	/**
+	 * Another way to init new notification, if exist one EDIDSS notification
+	 */
+	@Override
+	public String initializeNewNotification() {
+		Patient p = patientHome.getInstance();
+		List<TbCase> cases = p.getCases();
+		TbCase caseEIDSS = null;
+		for (TbCase tbCase : cases){
+			if(tbCase.getLegacyId() != null){
+				caseEIDSS = tbCase;
+			}
+		}
+		if (caseEIDSS != null){
+			caseHome.setId(caseEIDSS.getId());
+			caseHome.find();
+			Contexts.getConversationContext().set("tbcase", caseHome.getInstance());
+			if (p.getBirthDate() != null)
+				updatePatientAge();
+			String ret = initWithoutNotifications();
+			caseHome.getInstance().setNotificationUnit(new Tbunit());
+			caseHome.getInstance().setNotifAddress(new Address());
+			caseHome.getInstance().setCurrentAddress(new Address());
+			Contexts.getConversationContext().set("tbcase", caseHome.getInstance());
+			return ret;
+		}
+		return super.initializeNewNotification();
+	}
+	
+	/**
+	 * Initialize a new notification
+	 */
+	public String initWithoutNotifications() {
+		if (initialized)
+			return "initialized";
+		
+		Patient p = patientHome.getInstance();
+		
+		if (caseHome.getInstance().getClassification() == null)
+			return "/cases/index.xhtml";
+		
+		if ((!patientHome.isManaged()) && 
+			(p.getName() == null) && (p.getMiddleName() == null) && (p.getLastName() == null) &&
+			(p.getBirthDate() == null))
+		return "patient-searching";
+		
+		if (p.getBirthDate() != null)
+			updatePatientAge();
+		
+		// initialize default values
+		UserWorkspace userWorkspace = (UserWorkspace)Component.getInstance("userWorkspace");
+		if (userWorkspace != null) {
+			if (userWorkspace.getTbunit().isNotifHealthUnit())
+				getTbunitselection().setTbunit(userWorkspace.getTbunit());
+			
+			AdministrativeUnit au = userWorkspace.getAdminUnit();
+			if (au == null)
+				au = userWorkspace.getTbunit().getAdminUnit();
+			
+			if (au != null) {
+				au = entityManager.find(AdministrativeUnit.class, au.getId());
+				
+				getNotifAdminUnit().setSelectedUnit(au);
+
+				if (getTbunitselection().getTbunit() == null) {
+					List<AdministrativeUnit> lst = getTbunitselection().getAdminUnits();
+					
+					if (lst != null)
+					for (AdministrativeUnit adminUnit: lst) {
+						if (adminUnit.isSameOrChildCode(au.getCode())) {
+							getTbunitselection().setAdminUnit(adminUnit);
+						}
+					}					
+				}
+			}
+		}
+		initialized = true;
+		return "initialized";
 	}
 }
