a73faf408a326265bcb14cea572908d94030a750
vani <vani@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2012-05-29 20:58:40 +0000

    Cambodia[Customization]- Adding New files for Cambodia customization
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@1234 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

---
+++ src/kh/org/msh/tb/kh/entities/TbCaseKH.java
@@ -0,0 +1,56 @@
+package org.msh.tb.kh.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import org.msh.tb.entities.FieldValue;
+import org.msh.tb.entities.TbCase;
+import org.msh.tb.transactionlog.PropertyLog;
+
+@Entity
+@Table(name="tbcasekh")
+public class TbCaseKH extends TbCase {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4502733327442767580L;
+	
+	//@OneToMany(cascade={CascadeType.MERGE, CascadeType.PERSIST}, mappedBy="tbcase")
+	//@PropertyLog(ignore=true)
+	//private List<CaseDispensing_Kh> dispkh = new ArrayList<CaseDispensing_Kh>();
+	
+	@OneToMany(cascade={CascadeType.ALL}, mappedBy="tbcasekh")
+	private List<CaseSideEffectKH> sideEffectsKh = new ArrayList<CaseSideEffectKH>();
+	
+	
+	@Override
+	public CaseSideEffectKH findSideEffectData(FieldValue sideEffect) {
+		for (CaseSideEffectKH se: getSideEffectsKh()) {
+			if (se.getSideEffect().equals(sideEffect))
+				return se;
+		}
+		return null;
+	}
+
+//	public List<CaseDispensing_Kh> getDispkh() {
+//		return dispkh;
+//	}
+//
+//	public void setDispkh(List<CaseDispensing_Kh> dispkh) {
+//		this.dispkh = dispkh;
+//	}
+
+	public List<CaseSideEffectKH> getSideEffectsKh() {
+		return sideEffectsKh;
+	}
+	
+	public void setSideEffectsKh(List<CaseSideEffectKH> sideEffectsKh) {
+		this.sideEffectsKh = sideEffectsKh;
+	}
+}
