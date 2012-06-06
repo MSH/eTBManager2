4cc1425aa08db7448b08e724a11fafa4b6826a99
vani <vani@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2012-06-06 19:19:28 +0000

    Cambodia[Improvement, Exams] - adding a new custom class for ExamDST to include SampleType
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@1290 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

---
+++ src/kh/org/msh/tb/kh/entities/ExamDST_Kh.java
@@ -0,0 +1,27 @@
+package org.msh.tb.kh.entities;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+import org.msh.tb.entities.ExamDST;
+import org.msh.tb.entities.enums.SampleType;
+
+@Entity
+@DiscriminatorValue("kh")
+public class ExamDST_Kh extends ExamDST{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4683305582959181355L;
+	
+	private SampleType sampleType;
+
+	public SampleType getSampleType() {
+		return sampleType;
+	}
+	
+	public void setSampleType(SampleType sampleType) {
+		this.sampleType = sampleType;
+	}
+}
