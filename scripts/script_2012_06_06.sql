b8c7c5c10ab083d6d705ec6494dec74c7741134d
okurasov <okurasov@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2012-06-10 09:42:14 +0000

    AZ Improvement. New fields in TBCaseAZ
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@1317 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

---
+++ scripts/script_2012_06_06.sql
@@ -0,0 +1,34 @@
+/***************************************************************
+* By Oleksii Kurasov - 05-06-2012
+* 
+* Add customId values to azerbaijan fields of pulmonary end extrapulmonary subtypes in same order, that in report
+* 
+****************************************************************/
+
+
+UPDATE `etbmanager`.`fieldvalue` SET `displayOrder`='1' WHERE `id`='939292';
+UPDATE `etbmanager`.`fieldvalue` SET `displayOrder`='2' WHERE `id`='939293';
+UPDATE `etbmanager`.`fieldvalue` SET `displayOrder`='3' WHERE `id`='939294';
+UPDATE `etbmanager`.`fieldvalue` SET `displayOrder`='4' WHERE `id`='939295';
+UPDATE `etbmanager`.`fieldvalue` SET `displayOrder`='5' WHERE `id`='939296';
+UPDATE `etbmanager`.`fieldvalue` SET `displayOrder`='6' WHERE `id`='939297';
+UPDATE `etbmanager`.`fieldvalue` SET `displayOrder`='7' WHERE `id`='939298';
+UPDATE `etbmanager`.`fieldvalue` SET `displayOrder`='8' WHERE `id`='939291';
+UPDATE `etbmanager`.`fieldvalue` SET `displayOrder`='9' WHERE `id`='939289';
+
+UPDATE `etbmanager`.`fieldvalue` SET `displayOrder`='1' WHERE `id`='939301';
+UPDATE `etbmanager`.`fieldvalue` SET `displayOrder`='2' WHERE `id`='939302';
+UPDATE `etbmanager`.`fieldvalue` SET `displayOrder`='3' WHERE `id`='939300';
+UPDATE `etbmanager`.`fieldvalue` SET `displayOrder`='4' WHERE `id`='939303';
+UPDATE `etbmanager`.`fieldvalue` SET `displayOrder`='5' WHERE `id`='939305';
+UPDATE `etbmanager`.`fieldvalue` SET `displayOrder`='6' WHERE `id`='939306';
+UPDATE `etbmanager`.`fieldvalue` SET `displayOrder`='7' WHERE `id`='939308';
+UPDATE `etbmanager`.`fieldvalue` SET `displayOrder`='8' WHERE `id`='939304';
+UPDATE `etbmanager`.`fieldvalue` SET `displayOrder`='9' WHERE `id`='939307';
+UPDATE `etbmanager`.`fieldvalue` SET `displayOrder`='10' WHERE `id`='939309';
+UPDATE `etbmanager`.`fieldvalue` SET `displayOrder`='11' WHERE `id`='939290';
+UPDATE `etbmanager`.`fieldvalue` SET `displayOrder`='12' WHERE `id`='939310';
+
+UPDATE `etbmanager`.`fieldvalue` SET `field`=14 WHERE `id`='939290';
+
+
