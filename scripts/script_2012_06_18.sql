df00c546260ba3c5fe8e0607acaaa8aef9f054f1
rmemoria <rmemoria@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2012-06-18 15:24:48 +0000

    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@1364 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

---
+++ scripts/script_2012_06_18.sql
@@ -0,0 +1,8 @@
+/***************************************************************
+* By Ricardo Memoria
+* 
+* Including a new theme
+* 
+****************************************************************/
+
+insert into uitheme (name, systemTheme, defaultTheme, path) values ('Light Gray', true, false, 'gray');
