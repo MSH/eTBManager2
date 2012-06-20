3c1b184b3b29509e43c4ee15a9098feab3f902b8
vani <vani@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2012-06-20 15:47:58 +0000

    Cambodia[Fix] - correction in annotation names and adding the methods related to culture results.
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@1386 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

--- src/kh/org/msh/tb/kh/ExamCultureKHHome.java
+++ src/kh/org/msh/tb/kh/ExamCultureKHHome.java
@@ -1,14 +1,21 @@
 package org.msh.tb.kh;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.model.SelectItem;
+
 import org.jboss.seam.annotations.Factory;
 import org.jboss.seam.annotations.Name;
 import org.msh.tb.cases.exams.LaboratoryExamHome;
+import org.msh.tb.entities.ExamCulture;
+import org.msh.tb.entities.enums.CultureResult;
 import org.msh.tb.kh.entities.ExamCulture_Kh;
 
 import org.msh.tb.transactionlog.LogInfo;
 
-@Name("examCultureHomeKh")
-@LogInfo(roleName="EXAM_HIV")
+@Name("examCultureKHHome")
+@LogInfo(roleName="EXAM_CULTURE")
 public class ExamCultureKHHome extends LaboratoryExamHome<ExamCulture_Kh>  {
 
 	/**
@@ -16,8 +23,64 @@ public class ExamCultureKHHome extends LaboratoryExamHome<ExamCulture_Kh>  {
 	 */
 	private static final long serialVersionUID = -1298829141838748484L;
 	
-	@Factory("examCulturekh")
+	@Factory("examCulture_Kh")
 	public ExamCulture_Kh getExamCulture_Kh() {
 		return getInstance();
+	}
+	
+	private List<SelectItem> numColonies;
+	@Override
+	public String persist() {
+		ExamCulture exam = getInstance();
+		if (exam.getResult() != CultureResult.POSITIVE)
+			exam.setNumberOfColonies(null);
+		
+		return super.persist();
+	}
+
+
+	public List<SelectItem> getNumColonies() {
+		if (numColonies == null) {
+			numColonies = new ArrayList<SelectItem>();
+			
+			SelectItem item = new SelectItem();
+			item.setLabel("-");
+			numColonies.add(item);
+			
+			for (int i = 1; i <= 9; i++) {
+				item = new SelectItem();
+				item.setLabel(Integer.toString(i));
+				item.setValue(i);
+				numColonies.add(item);
+			}
+		}
+		return numColonies;
+	}
+	
+	/**
+	 * Create list of NumColonies of required count
+	 * */
+	
+	public List<SelectItem> getNumColonies(int count) {
+		if (numColonies == null) {
+			numColonies = new ArrayList<SelectItem>();
+			
+			SelectItem item = new SelectItem();
+			item.setLabel("-");
+			numColonies.add(item);
+			
+			for (int i = 1; i <= count; i++) {
+				item = new SelectItem();
+				item.setLabel(Integer.toString(i));
+				item.setValue(i);
+				numColonies.add(item);
+			}
+		}
+		return numColonies;
+	}
+
+	@Override
+	public String getJoinFetchHQL() {
+		return super.getJoinFetchHQL() + " left join fetch exam.method met ";
 	}
 }
