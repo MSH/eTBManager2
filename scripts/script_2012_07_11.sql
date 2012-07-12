1ec5d6598ff8ac857f8b2318289070b7da2cf249
okurasov <okurasov@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2012-07-12 08:53:47 +0000

    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@1486 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

--- scripts/script_2012_07_11.sql
+++ scripts/script_2012_07_11.sql
@@ -4,4 +4,4 @@
 * Rename russian lang admin unit category to azeri lang
 * 
 ****************************************************************/
-UPDATE `etbmanager`.`countrystructure` SET `name1`='Rayon və şəhər' WHERE `WORKSPACE_ID`='8
+UPDATE `etbmanager`.`countrystructure` SET `name1`='Rayon və şəhər' WHERE `WORKSPACE_ID`='8'
