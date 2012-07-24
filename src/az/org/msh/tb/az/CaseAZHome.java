8e85a47276d606186c6efd518abbf72952e8f625
okurasov <okurasov@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2012-07-24 14:09:53 +0000

    AZ additional data
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@1547 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

--- src/az/org/msh/tb/az/CaseAZHome.java
+++ src/az/org/msh/tb/az/CaseAZHome.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.msh.tb.az.entities.CaseSeverityMark;
+import org.msh.tb.az.entities.TbCaseAZ;
 import org.msh.tb.cases.CaseEditingHome;
 import org.msh.tb.cases.CaseHome;
 
 @Name("caseAZHome")
 public class CaseAZHome {
-
+	private String notifEIDSS="";
+	private String addrEIDSS="";
 	@In CaseHome caseHome;
 	@In(create=true) CaseEditingAZHome caseEditingAZHome;
 	@In(create=true) CaseEditingHome caseEditingHome;
@@ -34,7 +36,7 @@ public class CaseAZHome {
 		
 		return res;
 	}
-
+	
 
 	public List<CaseSeverityMark> getCaseSeverityMarks() {
 		if (!caseSeverityMarksHome.isEditing())
@@ -43,5 +45,25 @@ public class CaseAZHome {
 		return caseSeverityMarksHome.getSeverityMarks();
 	}
 	
-	
+	public TbCaseAZ getTbCase() {
+		return (TbCaseAZ)caseHome.getInstance();
+	}
+	public String getNotifEIDSS(){
+		TbCaseAZ c=getTbCase();
+		String s=c.getEIDSSComment();
+		if (s!=null){
+			String [] parts=s.split("/", 5);
+			notifEIDSS= parts[0];	
+		}
+			return notifEIDSS;
+	}
+	public String getAddrEIDSS(){
+		TbCaseAZ c=getTbCase();
+		String s=c.getEIDSSComment();
+		if (s!=null){
+			String [] parts=s.split("/", 5);
+			addrEIDSS= parts[1];	
+		}
+			return addrEIDSS;
+	}
 }
