2f342264ec805bfbff8d3270fcf0baa91665e536
vani <vani@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2012-08-24 21:28:55 +0000

    Bangladesh[Fix, Reports] - TB11Form - Adding order by fields
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@1689 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

--- src/bd/org/msh/tb/bd/PulmonaryRetreatIndicator.java
+++ src/bd/org/msh/tb/bd/PulmonaryRetreatIndicator.java
@@ -32,6 +32,7 @@ public class PulmonaryRetreatIndicator extends Indicator2D{
 		//String cond = " c.patientType <> 0 and c.state > 2 ";
 		String cond = " c.patientType <> 0 ";
 		setCondition(cond);
+		setOrderByFields("e.tbcase.id, e.dateCollected");
 		List<TbCase> lst = new ArrayList<TbCase>();
 		 lst = createQuery().getResultList();
 
