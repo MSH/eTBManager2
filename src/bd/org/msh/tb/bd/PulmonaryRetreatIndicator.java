1b155c2476eeb0823865833f7c0a73eda4e31873
vani <vani@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2012-08-24 19:42:40 +0000

    Bangladesh[Fix, Reports] - TB11Form - Implementing 'Not Evaluated' column and minor fix due to change in functional specification.
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@1684 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

--- src/bd/org/msh/tb/bd/PulmonaryRetreatIndicator.java
+++ src/bd/org/msh/tb/bd/PulmonaryRetreatIndicator.java
@@ -1,6 +1,5 @@
 package org.msh.tb.bd;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -25,18 +24,17 @@ public class PulmonaryRetreatIndicator extends Indicator2D{
 	 * 
 	 */
 	private static final long serialVersionUID = 2205664398466084869L;
-	private boolean flag = false;
 
 	@Override
 	protected void createIndicators() {
 		// TODO Auto-generated method stub
-		//Fetching all confirmed cases notified as 'retreat' and closed
-		String cond = " c.patientType <> 0 and c.state > 2 ";
+		//Fetching all confirmed cases notified as 'retreat' 
+		//String cond = " c.patientType <> 0 and c.state > 2 ";
+		String cond = " c.patientType <> 0 ";
 		setCondition(cond);
 		List<TbCase> lst = new ArrayList<TbCase>();
 		 lst = createQuery().getResultList();
 
-		System.out.println("List Size--->" +lst.size());
 		float cntRelM = 0, cntRelF = 0, cntFailM = 0, cntFailF = 0, cntDefM = 0, cntDefF = 0, cntOtherM = 0, cntOtherF = 0;
 		float cntCureRelM = 0, cntCureFailM = 0, cntCureDefM = 0, cntCureOtherM = 0, cntCureRelF = 0,  cntCureFailF = 0, cntCureDefF = 0, cntCureOtherF = 0;
 		float cntTreatCompRelM = 0, cntTreatCompFailM = 0, cntTreatCompDefM = 0, cntTreatCompOtherM = 0, cntTreatCompRelF = 0, cntTreatCompFailF = 0, cntTreatCompDefF = 0,cntTreatCompOtherF = 0; 
@@ -44,12 +42,17 @@ public class PulmonaryRetreatIndicator extends Indicator2D{
 		float cntFailRelM = 0, cntFailFailM = 0, cntFailDefM = 0, cntFailOtherM = 0, cntFailRelF = 0, cntFailFailF = 0, cntFailDefF = 0, cntFailOtherF = 0;
 		float cntDefRelM = 0, cntDefFailM = 0, cntDefDefM = 0, cntDefOtherM = 0, cntDefRelF = 0, cntDefFailF = 0, cntDefDefF = 0,cntDefOtherF = 0;
 		float cntTranRelM = 0, cntTranFailM = 0, cntTranDefM = 0, cntTranOtherM = 0, cntTranRelF = 0, cntTranFailF = 0, cntTranDefF = 0, cntTranOtherF = 0;
+		float cntNotEvalRelM = 0, cntNotEvalFailM = 0, cntNotEvalDefM = 0, cntNotEvalOtherM = 0;
+		float cntNotEvalRelF = 0, cntNotEvalFailF = 0, cntNotEvalDefF = 0, cntNotEvalOtherF = 0;
 		
 		for(TbCase val:lst){
-			
+			/*
+			 * TB 11 Report for Retreatment category - 'Relapse'
+			 */
 			if(val.getPatientType() == PatientType.RELAPSE){
 				if(val.getPatient().getGender() == Gender.MALE){
 					cntRelM++;
+					if(val.getState().ordinal()>2){
 					if(val.getState() == CaseState.CURED)
 						cntCureRelM++;
 					if(val.getState() == CaseState.TREATMENT_COMPLETED)
@@ -62,9 +65,13 @@ public class PulmonaryRetreatIndicator extends Indicator2D{
 						cntDefRelM++;
 					if(val.getState() == CaseState.TRANSFERRED_OUT)
 						cntTranRelM++;
+					}
+					else
+						cntNotEvalRelM++;
 				}
 				if(val.getPatient().getGender() == Gender.FEMALE){
 					cntRelF++;
+					if(val.getState().ordinal()>2){
 					if(val.getState() == CaseState.CURED)
 						cntCureRelF++;
 					if(val.getState() == CaseState.TREATMENT_COMPLETED)
@@ -77,105 +84,132 @@ public class PulmonaryRetreatIndicator extends Indicator2D{
 						cntDefRelF++;
 					if(val.getState() == CaseState.TRANSFERRED_OUT)
 						cntTranRelF++;
+					}
+					else
+						cntNotEvalRelF++;
 				}
 			}
 			
 			if(val.getPatientType() == PatientType.FAILURE_FT || val.getPatientType() == PatientType.FAILURE_RT){
 				if(val.getPatient().getGender() == Gender.MALE){
 					cntFailM++;
-					if(val.getState() == CaseState.CURED)
-						cntCureFailM++;
-					if(val.getState() == CaseState.TREATMENT_COMPLETED)
-						cntTreatCompFailM++;
-					if(val.getState() == CaseState.DIED)
-						cntDiedFailM++;
-					if(val.getState() == CaseState.FAILED)
-						cntFailFailM++;
-					if(val.getState() == CaseState.DEFAULTED)
-						cntDefFailM++;
-					if(val.getState() == CaseState.TRANSFERRED_OUT)
-						cntTranFailM++;
+					if(val.getState().ordinal()>2){
+						if(val.getState() == CaseState.CURED)
+							cntCureFailM++;
+						if(val.getState() == CaseState.TREATMENT_COMPLETED)
+							cntTreatCompFailM++;
+						if(val.getState() == CaseState.DIED)
+							cntDiedFailM++;
+						if(val.getState() == CaseState.FAILED)
+							cntFailFailM++;
+						if(val.getState() == CaseState.DEFAULTED)
+							cntDefFailM++;
+						if(val.getState() == CaseState.TRANSFERRED_OUT)
+							cntTranFailM++;
+						}
+						else
+							cntNotEvalFailM++;
 				}
 				if(val.getPatient().getGender() == Gender.FEMALE){
 					cntFailF++;
-					if(val.getState() == CaseState.CURED)
-						cntCureFailF++;
-					if(val.getState() == CaseState.TREATMENT_COMPLETED)
-						cntTreatCompFailF++;
-					if(val.getState() == CaseState.DIED)
-						cntDiedFailF++;
-					if(val.getState() == CaseState.FAILED)
-						cntFailFailF++;
-					if(val.getState() == CaseState.DEFAULTED)
-						cntDefFailF++;
-					if(val.getState() == CaseState.TRANSFERRED_OUT)
-						cntTranFailF++;
+					if(val.getState().ordinal()>2){
+						if(val.getState() == CaseState.CURED)
+							cntCureFailF++;
+						if(val.getState() == CaseState.TREATMENT_COMPLETED)
+							cntTreatCompFailF++;
+						if(val.getState() == CaseState.DIED)
+							cntDiedFailF++;
+						if(val.getState() == CaseState.FAILED)
+							cntFailFailF++;
+						if(val.getState() == CaseState.DEFAULTED)
+							cntDefFailF++;
+						if(val.getState() == CaseState.TRANSFERRED_OUT)
+							cntTranFailF++;
+						}
+					else
+						cntNotEvalFailF++;
 				}
 			}
 			
 			if(val.getPatientType() == PatientType.AFTER_DEFAULT){
 				if(val.getPatient().getGender() == Gender.MALE){
 					cntDefM++;
-					if(val.getState() == CaseState.CURED)
-						cntCureDefM++;
-					if(val.getState() == CaseState.TREATMENT_COMPLETED)
-						cntTreatCompDefM++;
-					if(val.getState() == CaseState.DIED)
-						cntDiedDefM++;
-					if(val.getState() == CaseState.FAILED)
-						cntFailDefM++;
-					if(val.getState() == CaseState.DEFAULTED)
-						cntDefDefM++;
-					if(val.getState() == CaseState.TRANSFERRED_OUT)
-						cntTranDefM++;
+					if(val.getState().ordinal()>2){
+						if(val.getState() == CaseState.CURED)
+							cntCureDefM++;
+						if(val.getState() == CaseState.TREATMENT_COMPLETED)
+							cntTreatCompDefM++;
+						if(val.getState() == CaseState.DIED)
+							cntDiedDefM++;
+						if(val.getState() == CaseState.FAILED)
+							cntFailDefM++;
+						if(val.getState() == CaseState.DEFAULTED)
+							cntDefDefM++;
+						if(val.getState() == CaseState.TRANSFERRED_OUT)
+							cntTranDefM++;
+						}
+					else
+						cntNotEvalDefM++;
 				}
 				if(val.getPatient().getGender() == Gender.FEMALE){
 					cntDefF++;
-					if(val.getState() == CaseState.CURED)
-						cntCureDefF++;
-					if(val.getState() == CaseState.TREATMENT_COMPLETED)
-						cntTreatCompDefF++;
-					if(val.getState() == CaseState.DIED)
-						cntDiedDefF++;
-					if(val.getState() == CaseState.FAILED)
-						cntFailDefF++;
-					if(val.getState() == CaseState.DEFAULTED)
-						cntDefDefF++;
-					if(val.getState() == CaseState.TRANSFERRED_OUT)
-						cntTranDefF++;
+					if(val.getState().ordinal()>2){
+						if(val.getState() == CaseState.CURED)
+							cntCureDefF++;
+						if(val.getState() == CaseState.TREATMENT_COMPLETED)
+							cntTreatCompDefF++;
+						if(val.getState() == CaseState.DIED)
+							cntDiedDefF++;
+						if(val.getState() == CaseState.FAILED)
+							cntFailDefF++;
+						if(val.getState() == CaseState.DEFAULTED)
+							cntDefDefF++;
+						if(val.getState() == CaseState.TRANSFERRED_OUT)
+							cntTranDefF++;
+						}
+					else
+						cntNotEvalDefF++;
 				}
 			}
 				
 				if(val.getPatientType() == PatientType.OTHER){
 					if(val.getPatient().getGender() == Gender.MALE){
 						cntOtherM++;
-						if(val.getState() == CaseState.CURED)
-							cntCureOtherM++;
-						if(val.getState() == CaseState.TREATMENT_COMPLETED)
-							cntTreatCompOtherM++;
-						if(val.getState() == CaseState.DIED)
-							cntDiedOtherM++;
-						if(val.getState() == CaseState.FAILED)
-							cntFailOtherM++;
-						if(val.getState() == CaseState.DEFAULTED)
-							cntDefOtherM++;
-						if(val.getState() == CaseState.TRANSFERRED_OUT)
-							cntTranOtherM++;
+						if(val.getState().ordinal()>2){
+							if(val.getState() == CaseState.CURED)
+								cntCureOtherM++;
+							if(val.getState() == CaseState.TREATMENT_COMPLETED)
+								cntTreatCompOtherM++;
+							if(val.getState() == CaseState.DIED)
+								cntDiedOtherM++;
+							if(val.getState() == CaseState.FAILED)
+								cntFailOtherM++;
+							if(val.getState() == CaseState.DEFAULTED)
+								cntDefOtherM++;
+							if(val.getState() == CaseState.TRANSFERRED_OUT)
+								cntTranOtherM++;
+							}
+						else
+							cntNotEvalOtherM++;
 					}
 					if(val.getPatient().getGender() == Gender.FEMALE){
 						cntOtherF++;
-						if(val.getState() == CaseState.CURED)
-							cntCureOtherF++;
-						if(val.getState() == CaseState.TREATMENT_COMPLETED)
-							cntTreatCompOtherF++;
-						if(val.getState() == CaseState.DIED)
-							cntDiedOtherF++;
-						if(val.getState() == CaseState.FAILED)
-							cntFailOtherF++;
-						if(val.getState() == CaseState.DEFAULTED)
-							cntDefOtherF++;
-						if(val.getState() == CaseState.TRANSFERRED_OUT)
-							cntTranOtherF++;
+						if(val.getState().ordinal()>2){
+							if(val.getState() == CaseState.CURED)
+								cntCureOtherF++;
+							if(val.getState() == CaseState.TREATMENT_COMPLETED)
+								cntTreatCompOtherF++;
+							if(val.getState() == CaseState.DIED)
+								cntDiedOtherF++;
+							if(val.getState() == CaseState.FAILED)
+								cntFailOtherF++;
+							if(val.getState() == CaseState.DEFAULTED)
+								cntDefOtherF++;
+							if(val.getState() == CaseState.TRANSFERRED_OUT)
+								cntTranOtherF++;
+							}
+						else
+							cntNotEvalOtherF++;
 					}
 			}
 		}
@@ -266,11 +300,23 @@ public class PulmonaryRetreatIndicator extends Indicator2D{
 		addValue(messages.get("manag.gender.male6"), messages.get("manag.pulmonary.default"), cntTranDefM);
 		addValue(messages.get("manag.gender.male6"), messages.get("manag.pulmonary.others"), cntTranOtherM);
 		
-		//Case State Transferred Out - Male
+		//Case State Transferred Out - FeMale
 		addValue(messages.get("manag.gender.female6"), messages.get("manag.pulmonary.relapse"), cntTranRelF);
 		addValue(messages.get("manag.gender.female6"), messages.get("manag.pulmonary.failures"), cntTranFailF);
 		addValue(messages.get("manag.gender.female6"), messages.get("manag.pulmonary.default"), cntTranDefF);
 		addValue(messages.get("manag.gender.female6"), messages.get("manag.pulmonary.others"), cntTranOtherF);
+		
+		//Not Eval - Male
+		addValue(messages.get("manag.gender.male7"), messages.get("manag.pulmonary.relapse"), cntNotEvalRelM);
+		addValue(messages.get("manag.gender.male7"), messages.get("manag.pulmonary.failures"), cntNotEvalFailM);
+		addValue(messages.get("manag.gender.male7"), messages.get("manag.pulmonary.default"), cntNotEvalDefM);
+		addValue(messages.get("manag.gender.male7"), messages.get("manag.pulmonary.others"), cntNotEvalOtherM);
+		
+		//Not Eval - FeMale
+		addValue(messages.get("manag.gender.female7"), messages.get("manag.pulmonary.relapse"), cntNotEvalRelF);
+		addValue(messages.get("manag.gender.female7"), messages.get("manag.pulmonary.failures"), cntNotEvalFailF);
+		addValue(messages.get("manag.gender.female7"), messages.get("manag.pulmonary.default"), cntNotEvalDefF);
+		addValue(messages.get("manag.gender.female7"), messages.get("manag.pulmonary.others"), cntNotEvalOtherF);
 		
 		float totRelM = 0, totRelF = 0, totFailM = 0, totFailF = 0, totDefM = 0, totDefF = 0, totOtherM = 0, totOtherF = 0;
 		totRelM = cntCureRelM + cntTreatCompRelM + cntDiedRelM + cntFailRelM + cntDefRelM + cntTranRelM;
@@ -285,15 +331,15 @@ public class PulmonaryRetreatIndicator extends Indicator2D{
 		totOtherM = cntCureOtherM + cntTreatCompOtherM + cntDiedOtherM + cntFailOtherM + cntDefOtherM + cntTranOtherM;
 		totOtherF = cntCureOtherF + cntTreatCompOtherF + cntDiedOtherF + cntFailOtherF + cntDefOtherF + cntTranOtherF;
 		
-		addValue(messages.get("manag.gender.male7"), messages.get("manag.pulmonary.relapse"), totRelM);
-		addValue(messages.get("manag.gender.male7"), messages.get("manag.pulmonary.failures"), totFailM);
-		addValue(messages.get("manag.gender.male7"), messages.get("manag.pulmonary.default"), totDefM);
-		addValue(messages.get("manag.gender.male7"), messages.get("manag.pulmonary.others"), totOtherM);
+		addValue(messages.get("manag.gender.male8"), messages.get("manag.pulmonary.relapse"), totRelM);
+		addValue(messages.get("manag.gender.male8"), messages.get("manag.pulmonary.failures"), totFailM);
+		addValue(messages.get("manag.gender.male8"), messages.get("manag.pulmonary.default"), totDefM);
+		addValue(messages.get("manag.gender.male8"), messages.get("manag.pulmonary.others"), totOtherM);
 		
-		addValue(messages.get("manag.gender.female7"), messages.get("manag.pulmonary.relapse"), totRelF);
-		addValue(messages.get("manag.gender.female7"), messages.get("manag.pulmonary.failures"), totFailF);
-		addValue(messages.get("manag.gender.female7"), messages.get("manag.pulmonary.default"), totDefF);
-		addValue(messages.get("manag.gender.female7"), messages.get("manag.pulmonary.others"), totOtherF);
+		addValue(messages.get("manag.gender.female8"), messages.get("manag.pulmonary.relapse"), totRelF);
+		addValue(messages.get("manag.gender.female8"), messages.get("manag.pulmonary.failures"), totFailF);
+		addValue(messages.get("manag.gender.female8"), messages.get("manag.pulmonary.default"), totDefF);
+		addValue(messages.get("manag.gender.female8"), messages.get("manag.pulmonary.others"), totOtherF);
 		
 		addValue(messages.get("manag.pulmonary.tot"), messages.get("manag.pulmonary.relapse"), totRelM + totRelF);
 		addValue(messages.get("manag.pulmonary.tot"), messages.get("manag.pulmonary.failures"), totFailM + totFailF);
@@ -303,7 +349,6 @@ public class PulmonaryRetreatIndicator extends Indicator2D{
 	
 	@Override
 	public String getHQLSelect() {
-		//return "select c.id, c.state, c.patientType, c.pulmonaryType.id, c.pulmonaryType.shortName, c.patient.gender";
 		return "";
 	}	
 }
