c5d369946ca6debc8fed465757c455a7c10ee1bb
vani <vani@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2012-08-24 21:01:01 +0000

    Bangladesh[Fix, Reports] - TB10Form - Change in functional requirement. Smear Positive cases are now defined as those cases with a recorded first microscopy result(exams) as positive as against previously considered as cases with Pulmonary type recorded as positive during notification. (Similar for Smear Negative)
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@1688 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

--- src/bd/org/msh/tb/bd/TBForm10Block2.java
+++ src/bd/org/msh/tb/bd/TBForm10Block2.java
@@ -4,10 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
+import javax.persistence.EntityManager;
+
+import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.international.Messages;
 import org.msh.tb.entities.TbCase;
 import org.msh.tb.entities.enums.Gender;
+import org.msh.tb.entities.enums.MicroscopyResult;
 import org.msh.tb.entities.enums.PatientType;
 import org.msh.tb.indicators.core.Indicator;
 
@@ -18,75 +22,80 @@ public class TBForm10Block2 extends Indicator{
 	 * 
 	 */
 	private static final long serialVersionUID = -5835057642122065366L;
-	private boolean flag = false;
-
+	@In(create=true) EntityManager entityManager;
+	
 	@Override
 	protected void createIndicators() {
 		// TODO Auto-generated method stub
-		List<TbCase> lst = new ArrayList<TbCase>();
-		lst = createQuery().getResultList();
+		
+		List<Object[]> lst = createQuery().getResultList();
 		
 		float cntAgeRange1M = 0,cntAgeRange1F = 0, cntAgeRange2M = 0,cntAgeRange2F = 0, cntAgeRange3M = 0, cntAgeRange3F = 0;
 		float cntAgeRange4M = 0, cntAgeRange4F = 0, cntAgeRange5M = 0, cntAgeRange5F = 0, cntAgeRange6M = 0, cntAgeRange6F = 0;
 		float cntAgeRange7M = 0, cntAgeRange7F = 0,  cntAgeRange8M = 0, cntAgeRange8F = 0;
 		
-		for(TbCase val:lst){
-			//Checking for NEW Smear Positive
-			if(val.getPatientType() == PatientType.NEW && val.getPulmonaryType() != null && isSmearPos(val.getPulmonaryType().getShortName().toString())){
-				switch(getAgeRange(val)){
+		for(Object[] val:lst){
+			TbCase tbcase = new TbCase();
+			tbcase = entityManager.find(TbCase.class, val[0]);
+			
+			/*
+			 * Checking for 1st reported Microscopy result to be Positive
+			 */
+				if(val[5] == MicroscopyResult.PLUS || val[5] == MicroscopyResult.PLUS2 || val[5] == MicroscopyResult.PLUS3 || val[5] == MicroscopyResult.PLUS4) {
+				switch(getAgeRange(tbcase)){
 				case 1 :{
-					if(isMale(val.getPatient().getGender()))
+					if(tbcase.getPatient().getGender()==Gender.MALE)
 						cntAgeRange1M++;
-					if(isFemale(val.getPatient().getGender()))
+					if(tbcase.getPatient().getGender()==Gender.FEMALE)
 						cntAgeRange1F++;
 					break;
 					}
 				case 2 :{
-					if(isMale(val.getPatient().getGender()))
+					if(tbcase.getPatient().getGender()==Gender.MALE)
 						cntAgeRange2M++;
-					if(isFemale(val.getPatient().getGender()))
+					if(tbcase.getPatient().getGender()==Gender.FEMALE)
 						cntAgeRange2F++;
 					break;
 					}
 				case 3 : {
-					if(isMale(val.getPatient().getGender()))
+					if(tbcase.getPatient().getGender()==Gender.MALE)
 						cntAgeRange3M++;
-					if(isFemale(val.getPatient().getGender()))
+					if(tbcase.getPatient().getGender()==Gender.FEMALE)
 						cntAgeRange3F++;
 					break;
 					}
 				case 4 : {
-					if(isMale(val.getPatient().getGender()))
+					if(tbcase.getPatient().getGender()==Gender.MALE)
 						cntAgeRange4M++;
-					if(isFemale(val.getPatient().getGender()))
+					if(tbcase.getPatient().getGender()==Gender.FEMALE)
 						cntAgeRange4F++;
 					break; 
 					}
 				case 5 :{
-					if(isMale(val.getPatient().getGender()))
+					if(tbcase.getPatient().getGender()==Gender.MALE)
 						cntAgeRange5M++;
-					if(isFemale(val.getPatient().getGender()))
+					if(tbcase.getPatient().getGender()==Gender.FEMALE)
 						cntAgeRange5F++;
 					break;
 					}
 				case 6 : {
-					if(isMale(val.getPatient().getGender()))
+					if(tbcase.getPatient().getGender()==Gender.MALE)
 						cntAgeRange6M++;
-					if(isFemale(val.getPatient().getGender()))
+					if(tbcase.getPatient().getGender()==Gender.FEMALE)
 						cntAgeRange6F++;
 					break;
 					}
 				case 7 : {
-					if(isMale(val.getPatient().getGender()))
+					if(tbcase.getPatient().getGender()==Gender.MALE)
 						cntAgeRange7M++;
-					if(isFemale(val.getPatient().getGender()))
+					if(tbcase.getPatient().getGender()==Gender.FEMALE)
 						cntAgeRange7F++;
 					break; 
 					}
 				case 8 : {
-					if(isMale(val.getPatient().getGender()))
+					if(tbcase.getPatient().getGender()==Gender.MALE)
 						cntAgeRange8M++;
-					if(isFemale(val.getPatient().getGender()))
+					if(tbcase.getPatient().getGender()==Gender.FEMALE)
 						cntAgeRange8F++;
 					break; 
 					}
@@ -125,29 +134,7 @@ public class TBForm10Block2 extends Indicator{
 		addValue(messages.get("Gender.FEMALE"), messages.get("#"), cntTotF);
 		addValue(messages.get("manag.pulmonary.tot"), messages.get("#"), cntTotM + cntTotF);
 	}
-	
-	public boolean isSmearPos(String strSmear){
-		flag = false;
-		if(strSmear.equalsIgnoreCase("Smear(+)")){
-			flag = true;
-		}
-		return flag;		
-	}
-
-	public boolean isMale(Gender g){
-		flag = false;
-		if(g == Gender.MALE)
-				flag = true;
-		return flag;
-	}
-	
-	public boolean isFemale(Gender g){
-		flag = false;
-		if(g == Gender.FEMALE)
-				flag = true;
-		return flag;
-	}
-	
+		
 	public int getAgeRange(TbCase c ){
 		int age = c.getPatientAge();
 		int agerange = 0;
@@ -170,9 +157,19 @@ public class TBForm10Block2 extends Indicator{
 		
 		return agerange;
 	}
+	
 	@Override
 	public String getHQLSelect() {
-		return "";
+		String strSel = "";
+		strSel = "select c.id, c.patient.gender, c.state, c.outcomeDate, e.dateCollected, e.result ";
+		return strSel;
+	}
+	
+	@Override
+	protected String getHQLFrom() {
+		String strFrom = "";
+		strFrom =  " from ExamMicroscopy e join e.tbcase c ";
+		return strFrom;
 	}
 
 }
