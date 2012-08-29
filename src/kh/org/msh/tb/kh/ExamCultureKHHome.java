5a266c0fae5c9d9aa1d6c849443e14d20953f773
vani <vani@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2012-08-29 21:07:52 +0000

    Cambodia[Fix, Exams-Culture] - overriding persist() and remove()
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@1716 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

--- src/kh/org/msh/tb/kh/ExamCultureKHHome.java
+++ src/kh/org/msh/tb/kh/ExamCultureKHHome.java
@@ -4,24 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.model.SelectItem;
+import javax.persistence.EntityManager;
 
 import org.jboss.seam.annotations.Factory;
+import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
+import org.msh.tb.cases.CaseHome;
 import org.msh.tb.cases.exams.LaboratoryExamHome;
-import org.msh.tb.entities.ExamCulture;
 import org.msh.tb.entities.enums.CultureResult;
 import org.msh.tb.kh.entities.ExamCulture_Kh;
-
 import org.msh.tb.transactionlog.LogInfo;
 
 @Name("examCultureKHHome")
-@LogInfo(roleName="EXAM_CULTURE")
-public class ExamCultureKHHome extends LaboratoryExamHome<ExamCulture_Kh>  {
+@LogInfo(roleName="EXAM_CULTURE_KH")
+public class ExamCultureKHHome<E> extends LaboratoryExamHome<ExamCulture_Kh>  {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -1298829141838748484L;
+	@In(create=true)
+	private EntityManager entityManager;
+	@In(required=true) CaseHome caseHome;
 	
 	@Factory("examCulture_Kh")
 	public ExamCulture_Kh getExamCulture_Kh() {
@@ -31,14 +35,23 @@ public class ExamCultureKHHome extends LaboratoryExamHome<ExamCulture_Kh>  {
 	private List<SelectItem> numColonies;
 	@Override
 	public String persist() {
-		ExamCulture exam = getInstance();
+		ExamCulture_Kh exam = getInstance();
+		exam.setTbcase(caseHome.getInstance());
 		if (exam.getResult() != CultureResult.POSITIVE)
 			exam.setNumberOfColonies(null);
 		
-		return super.persist();
+		entityManager.persist(exam);
+		return "persisted";
 	}
 
-
+	@Override
+	public String remove() {
+		ExamCulture_Kh exam = getInstance();
+		exam.setTbcase(caseHome.getInstance());
+		entityManager.remove(exam);
+		return"exam-removed";
+	}
+	
 	public List<SelectItem> getNumColonies() {
 		if (numColonies == null) {
 			numColonies = new ArrayList<SelectItem>();
