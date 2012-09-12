9c56316c87c744c5787cdd71251f7a3c2fa79393
vani <vani@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2012-09-12 18:33:02 +0000

    Bangladesh - added bd specific 'site of infection' and 'drug resistance types'
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@1744 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

--- src/bd/org/msh/tb/bd/GlobalLists.java
+++ src/bd/org/msh/tb/bd/GlobalLists.java
@@ -1,5 +1,7 @@
 package org.msh.tb.bd;
 
+import org.apache.commons.beanutils.PropertyUtils;
+import org.jboss.seam.Component;
 import org.jboss.seam.annotations.Factory;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.intercept.BypassInterceptors;
@@ -8,7 +10,11 @@ import org.msh.tb.bd.entities.enums.DotProvider;
 import org.msh.tb.bd.entities.enums.Occupation;
 import org.msh.tb.bd.entities.enums.SalaryRange;
 import org.msh.tb.bd.entities.enums.SkinTestResult;
+import org.msh.tb.entities.enums.DrugResistanceType;
+import org.msh.tb.entities.enums.InfectionSite;
+import org.msh.tb.entities.enums.PatientType;
 import org.msh.tb.entities.enums.ReferredTo;
+import org.msh.tb.entities.enums.SampleType;
 import org.msh.tb.entities.enums.TbField;
 
 
@@ -64,8 +70,25 @@ public class GlobalLists {
 		TbField.PULMONARY_TYPES,
 		TbField.EXTRAPULMONARY_TYPES,
 		TbField.ADJUSTMENT
-	};	
+	};
 	
+	private static final SampleType sampleType[] = {
+		SampleType.SPUTUM,
+		SampleType.PUS,
+		SampleType.CSF,
+		SampleType.URINE,
+		SampleType.STOOL,
+		SampleType.TISSUE,
+		SampleType.OTHER
+	};
+	
+	private static final DrugResistanceType drugResistanceType[] = {
+		DrugResistanceType.MONO_RESISTANCE,
+		DrugResistanceType.POLY_RESISTANCE,
+		DrugResistanceType.MULTIDRUG_RESISTANCE,
+		DrugResistanceType.EXTENSIVEDRUG_RESISTANCE,
+		DrugResistanceType.UNKNOWN,
+	};
 	
 	public TbField[] getTbFields() {
 		return tbfields;
@@ -75,6 +98,26 @@ public class GlobalLists {
 		return referredToTypes;
 	}	
 	
+	
+	public static final InfectionSite infectionSite[] =  {
+		InfectionSite.PULMONARY,
+		InfectionSite.EXTRAPULMONARY
+	};
+	
+	@Factory("infectionSite_bd")
+	public InfectionSite[] getInfectionSite() {
+		return  infectionSite;
+	}
 
+	@Factory("sampleTypes_bd")
+	public static SampleType[] getSampletype() {
+		return sampleType;
+	}
+
+	@Factory("drugResistanceType_bd")
+	public static DrugResistanceType[] getDrugresistancetype() {
+		return drugResistanceType;
+	}
+	
 	
 }
