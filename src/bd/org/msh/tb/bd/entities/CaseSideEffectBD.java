dce16942f0691ae9533ff95127aedaff34ef631c
vani <vani@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2012-09-13 15:09:36 +0000

    Bangladesh[Improvement, Adverse Effect] - adding new file related to CaseSideEffect for bd workspace
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@1752 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

---
+++ src/bd/org/msh/tb/bd/entities/CaseSideEffectBD.java
@@ -0,0 +1,119 @@
+package org.msh.tb.bd.entities;
+
+import java.util.Date;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.validator.NotNull;
+import org.jboss.seam.annotations.Name;
+import org.msh.tb.entities.CaseSideEffect;
+import org.msh.tb.bd.entities.enums.SideEffectAction;
+import org.msh.tb.bd.entities.enums.SideEffectGrading;
+import org.msh.tb.bd.entities.enums.SideEffectOutcome;
+import org.msh.tb.bd.entities.enums.SideEffectSeriousness;
+import org.msh.utils.date.DateUtils;
+
+@Entity
+@DiscriminatorValue("bd")
+@Name("caseSideEffectBD")
+public class CaseSideEffectBD extends CaseSideEffect{
+
+	@ManyToOne
+	@JoinColumn(name="CASE_DATA_ID")
+	@NotNull
+	private TbCaseBD tbcasebd;	
+
+	private SideEffectGrading grade;
+	
+	private SideEffectAction actionTaken; 
+	
+	private SideEffectOutcome outcome;
+	
+	private SideEffectSeriousness seriousness;
+	
+	@Temporal(TemporalType.DATE)
+	private Date effectSt;
+
+	@Temporal(TemporalType.DATE)
+	private Date effectEnd;
+	
+	//private String comments;	
+	
+	public Integer getMonthOfTreatment(){
+		int i = 0;
+		i = DateUtils.monthsBetween(tbcasebd.getTreatmentPeriod().getIniDate(), effectSt)+1;
+		return i;
+		
+	}
+		
+	public SideEffectGrading getGrade() {
+		return grade;
+	}
+
+	public void setGrade(SideEffectGrading grade) {
+		this.grade = grade;
+	}
+
+	public SideEffectAction getActionTaken() {
+		return actionTaken;
+	}
+
+	public void setActionTaken(SideEffectAction actionTaken) {
+		this.actionTaken = actionTaken;
+	}
+
+	public SideEffectOutcome getOutcome() {
+		return outcome;
+	}
+
+	public void setOutcome(SideEffectOutcome outcome) {
+		this.outcome = outcome;
+	}
+
+	public SideEffectSeriousness getSeriousness() {
+		return seriousness;
+	}
+
+	public void setSeriousness(SideEffectSeriousness seriousness) {
+		this.seriousness = seriousness;
+	}
+
+	public TbCaseBD getTbcasebd() {
+		return tbcasebd;
+	}
+
+	public void setTbcasebd(TbCaseBD tbcasebd) {
+		this.tbcasebd = tbcasebd;
+	}
+
+//	public String getComments() {
+//		return comments;
+//	}
+//
+//	public void setComments(String comments) {
+//		this.comments = comments;
+//	}
+
+	public Date getEffectSt() {
+		return effectSt;
+	}
+
+	public void setEffectSt(Date effectSt) {
+		this.effectSt = effectSt;
+	}
+
+	public Date getEffectEnd() {
+		return effectEnd;
+	}
+
+	public void setEffectEnd(Date effectEnd) {
+		this.effectEnd = effectEnd;
+	}
+
+	
+}
