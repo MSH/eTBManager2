b8a2e41d879fefb00053b1061667dfc8d619c49a
vani <vani@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2012-09-13 14:34:29 +0000

    Bangladesh[Improvement, Notification] - adding a sub category for Pulmonary types
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@1748 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

--- src/bd/org/msh/tb/bd/entities/TbCaseBD.java
+++ src/bd/org/msh/tb/bd/entities/TbCaseBD.java
@@ -1,12 +1,20 @@
 package org.msh.tb.bd.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import org.msh.tb.bd.entities.enums.Occupation;
+import org.msh.tb.bd.entities.enums.PulmonaryTypesBD;
 import org.msh.tb.bd.entities.enums.SalaryRange;
+import org.msh.tb.entities.FieldValue;
 import org.msh.tb.entities.TbCase;
 import org.msh.tb.entities.enums.YesNoType;
+
 
 @Entity
 @Table(name="tbcasebd")
@@ -23,8 +31,10 @@ public class TbCaseBD extends TbCase{
 	
 	private SalaryRange salary;
 	
+	private PulmonaryTypesBD pulmonaryTypesBD;
 	
-	
+	@OneToMany(cascade={CascadeType.ALL}, mappedBy="tbcasebd")
+	private List<CaseSideEffectBD> sideEffectsBd = new ArrayList<CaseSideEffectBD>();
 	
 
 	public SalaryRange getSalary() {
@@ -51,11 +61,35 @@ public class TbCaseBD extends TbCase{
 		this.bcgScar = bcgScar;
 	}
 
-
-
-
-
+	/**
+	 * Search for side effect data by the side effect
+	 * @param sideEffect - FieldValue object representing the side effect
+	 * @return - CaseSideEffect instance containing side effect data of the case, or null if there is no side effect data
+	 */
+	@Override
+	public CaseSideEffectBD findSideEffectData(FieldValue sideEffect) {
+		for (CaseSideEffectBD se: getSideEffectsBd()) {
+			if (se.getSideEffect().getValue().equals(sideEffect))
+				return se;
+		}
+		return null;
+	}
 
 	
+	public List<CaseSideEffectBD> getSideEffectsBd() {
+		return sideEffectsBd;
+	}
 	
+	public void setSideEffectsBd(List<CaseSideEffectBD> sideEffectsBd) {
+		this.sideEffectsBd = sideEffectsBd;
+	}
+
+	public PulmonaryTypesBD getPulmonaryTypesBD() {
+		return pulmonaryTypesBD;
+	}
+	
+	public void setPulmonaryTypesBD(PulmonaryTypesBD pulmonaryTypesBD) {
+		this.pulmonaryTypesBD = pulmonaryTypesBD;
+	}
+		
 }
