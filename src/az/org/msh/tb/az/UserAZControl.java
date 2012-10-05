4fcb7b5eabb6ca5aa220515442ac4b2ca3e56e51
okurasov <okurasov@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2012-10-05 10:42:03 +0000

    AZ Improvement remove existent e-mail address check. Demanded by the customer
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@1865 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

--- src/az/org/msh/tb/az/UserAZControl.java
+++ src/az/org/msh/tb/az/UserAZControl.java
@@ -13,7 +13,7 @@ import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.international.Messages;
 
-@Name("userUAControl")
+@Name("userAZControl")
 public class UserAZControl{
 	@In(create=true) Map<String, String> messages;
 	
