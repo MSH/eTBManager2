1d4807f455cb55e6ee96457a318a55116fd8573d
rmemoria <rmemoria@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2012-10-22 18:29:49 +0000

    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@1931 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

--- src/action/org/msh/tb/cases/CaseStateReport.java
+++ src/action/org/msh/tb/cases/CaseStateReport.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
 
 import org.jboss.seam.Component;
 import org.jboss.seam.annotations.Name;
