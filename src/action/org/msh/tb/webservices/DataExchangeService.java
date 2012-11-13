bb08104ec9bc270dc866899ab8b6095080ab5fea
rmemoria <rmemoria@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2012-11-13 16:28:46 +0000

    * Generic [Bug] Web Service - Error when posting data - No transaction in progress * Generic [Improvement] Web Service - New class RemoteActionHandler to support basic operations in Web Services like serialization, transaction, authentication,etc.
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@1998 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

--- src/action/org/msh/tb/webservices/DataExchangeService.java
+++ src/action/org/msh/tb/webservices/DataExchangeService.java
@@ -1,52 +1,28 @@
 package org.msh.tb.webservices;
 
-import java.util.List;
-
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 import javax.jws.soap.SOAPBinding.Style;
-import javax.persistence.EntityManager;
 
 import org.jboss.seam.Component;
-import org.jboss.seam.faces.FacesMessages;
-import org.jboss.seam.international.StatusMessage;
-import org.jboss.seam.security.Identity;
-import org.msh.datastream.XmlDeserializer.ObjectReferenceable;
-import org.msh.tb.application.AppFacesMessages;
-import org.msh.tb.cases.CaseHome;
 import org.msh.tb.cases.exams.ExamDSTHome;
 import org.msh.tb.entities.ExamDST;
-import org.msh.tb.entities.TbCase;
-import org.msh.tb.login.AuthenticatorBean;
 
 @WebService(name="dataExchangeService", serviceName="dataExchangeService")
 @SOAPBinding(style=Style.RPC)
-public class DataExchangeService implements ObjectReferenceable {
+public class DataExchangeService {
 
 	@WebMethod
 	public String importData(@WebParam String sessionId, @WebParam String xmldata) {
-		Response resp = new Response();
-		try {
-
-			AuthenticatorBean authenticator = (AuthenticatorBean)Component.getInstance("authenticator");
-			authenticator.setSessionId(sessionId);
-			Identity.instance().login();
-			if (!Identity.instance().isLoggedIn()) {
-				resp.setErrorno(Response.RESP_INVALID_SESSION);
+		final String xml = xmldata;
+		return (new RemoteActionHandler(sessionId) {
+			@Override
+			protected Object execute() {
+				return importSessionData(this, xml);
 			}
-			else {
-				FacesMessages.instance().clear();
-				importSessionData(resp, xmldata);
-			}
-
-		} catch (Exception e) {
-			resp.setErrorno(Response.RESP_UNEXPECTED_ERROR);
-			resp.setErrormsg(e.toString());
-			e.printStackTrace();
-		}
-		return ObjectSerializer.serializeToXml(resp);
+		}).run();
 	}
 	
 	
@@ -55,50 +31,22 @@ public class DataExchangeService implements ObjectReferenceable {
 	 * @param resp
 	 * @param xmldata
 	 */
-	protected void importSessionData(Response resp, String xmldata) {
-		ExamDST exam = ObjectSerializer.deserializeFromXml(xmldata, ExamDST.class, this);
-		if (exam == null)
-			return;
+	protected Object importSessionData(RemoteActionHandler handler, String xmldata) {
+		ExamDST exam = handler.deserializeFromXml(xmldata, ExamDST.class);
+		if (exam == null) {
+			handler.setResponseError(Response.RESP_VALIDATION_ERROR, "No data found in xml");
+			return null;
+		}
 
 		ExamDSTHome home = (ExamDSTHome)Component.getInstance("examDSTHome", true);
 		home.setInstance(exam);
 
-
 		// try to save and check result
 		if (!("persisted".equals(home.persist()))) {
-			AppFacesMessages fm = (AppFacesMessages)FacesMessages.instance();
-
-			StringBuilder s = new StringBuilder();
-			for (StatusMessage msg: fm.getStatusMessages()) {
-				if (s.length() > 0)
-					s.append(", ");
-				msg.interpolate();
-				s.append(msg.getSummary());
-			}
-
-			resp.setErrormsg(s.toString());
-			resp.setErrorno(Response.RESP_VALIDATION_ERROR);
+			if (handler.checkValidationErrors())
+				return null;
 		}
+		return null;
 	}
 
-
-	/* (non-Javadoc)
-	 * @see org.msh.datastream.XmlDeserializer.ObjectReferenceable#objectByKey(java.lang.Class, java.lang.Object)
-	 */
-	@Override
-	public Object objectByKey(Class clazz, Object key) {
-		if (clazz == TbCase.class) {
-			CaseHome home = (CaseHome)Component.getInstance("caseHome", true);
-			home.setId(key);
-			return home.getInstance();
-		}
-		
-		EntityManager em = (EntityManager)Component.getInstance("entityManager");
-
-		List lst = em.createQuery("from " + clazz.getCanonicalName() + " where id = :id")
-			.setParameter("id", key)
-			.getResultList();
-
-		return (lst.size() > 0? lst.get(0) : null);
-	}
 }
---
+++ src/action/org/msh/tb/webservices/RemoteActionHandler.java
@@ -0,0 +1,280 @@
+package org.msh.tb.webservices;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.jboss.seam.Component;
+import org.jboss.seam.faces.FacesMessages;
+import org.jboss.seam.international.StatusMessage;
+import org.jboss.seam.security.Identity;
+import org.jboss.seam.transaction.UserTransaction;
+import org.msh.datastream.XmlSerializer;
+import org.msh.datastream.XmlDeserializer.ObjectReferenceable;
+import org.msh.tb.application.AppFacesMessages;
+import org.msh.tb.cases.CaseHome;
+import org.msh.tb.entities.TbCase;
+import org.msh.tb.login.AuthenticatorBean;
+
+/**
+ * Support for remote procedure calls (web services), handling authentication, transaction, error and preparation of response to the client.
+ * This class wraps common operations when dealing with remote calls from Web Services. This is an abstract class and its main usage is
+ * being declared as an anonymous class implementing the <code>execute();</code> method. The <code>execute()</code> method must implement
+ * the action to be performed, and its return value is serialized (to XML) and sent back to the client as a {@link Response} object.
+ * <p/> 
+ * The action wrapped by {@link RemoteActionHandler} is fired by calling the <code>run();</code> method. Inside the <code>run()</code> method, 
+ * the class will try to authenticate the client (if the sessionID) was provided, start a transaction (if the getTransaction() is true) 
+ * and call the <code>execute()</code> method. It also catches any exception thrown and wrap them inside the {@link Response} object
+ * sent back to the client.
+ * <p/>
+ * If the <code>sessionId</code> is not provided (by the constructor or the setSessionId() method), the authentication will not be done,
+ * and it's the responsibility of the action to authenticate or answer the client without authentication.
+ *   
+ * @author Ricardo Memoria
+ *
+ */
+public abstract class RemoteActionHandler implements ObjectReferenceable {
+
+	private String sessionId;
+	private Response response;
+	private UserTransaction transaction;
+	private boolean transactional= true;
+	
+	/**
+	 * Constructor of the class passing the client session identification as parameter. If the 
+	 * sessionId is provided, the class will try to authenticate the client before executing
+	 * the action
+	 * @param sessionId
+	 */
+	public RemoteActionHandler(String sessionId) {
+		super();
+		this.sessionId = sessionId;
+	}
+
+
+	/**
+	 * Default constructor of the class
+	 */
+	public RemoteActionHandler() {
+		super();
+	}
+
+
+	/**
+	 * Abstract method that should be implemented to execute the action of the remote call. This method
+	 * is never called directly, but by the <code>run()</code> method.
+	 * <p/>
+	 * @return The result of the action. This result will be serialized to XML using the {@link XmlSerializer} class
+	 * and make available as a response to the client by the return of the method <code>run()</code>
+	 */
+	protected abstract Object execute();
+
+
+	/**
+	 * Run the remote call handler, calling the <code>execute()</code> method. If information about user
+	 * authentication is supported, the system will call the authenticate method before. 
+	 * @return instance of the {@link Response} class already serialized to the client
+	 */
+	public String run() {
+		response = new Response();
+		try {
+			// authenticate first ?
+			if (sessionId != null) {
+				if (!authenticate())
+					return getSerializedResponse();
+			}
+
+			if (transactional)
+				beginTransaction();
+
+			// run the action
+			Object result = execute();
+			
+			if (transactional)
+				commitTransaction();
+
+			response.setResult(result);
+
+		} catch (Exception e) {
+			if (transactional)
+				rollbackTransaction();
+			setResponseError(Response.RESP_UNEXPECTED_ERROR, e.toString());
+		}
+		
+		return getSerializedResponse();
+	}
+
+	/**
+	 * Check if there are messages declared in the {@link FacesMessages} SEAM component. If there
+	 * is no message, the method returns false. If there are messages, the system returns
+	 * true and fills the response with the proper validation error messages  
+	 * @return
+	 */
+	protected boolean checkValidationErrors() {
+		AppFacesMessages fm = (AppFacesMessages)FacesMessages.instance();
+
+		List<StatusMessage> msgs = fm.getStatusMessages();
+		if (msgs.size() == 0)
+			return false;
+
+		StringBuilder s = new StringBuilder();
+		for (StatusMessage msg: msgs) {
+			if (s.length() > 0)
+				s.append(", ");
+			msg.interpolate();
+			s.append(msg.getSummary());
+		}
+
+		setResponseError(Response.RESP_VALIDATION_ERROR, s.toString());
+		return true;
+	}
+
+	/**
+	 * Set the error number and error msg of the response
+	 * @param errorno
+	 * @param errormsg
+	 */
+	public void setResponseError(int errorno, String errormsg) {
+		response.setErrorno(errorno);
+		response.setErrormsg(errormsg);
+	}
+
+	/**
+	 * Create a serialized XML of the response property
+	 * @return
+	 */
+	private String getSerializedResponse() {
+		return ObjectSerializer.serializeToXml(response);
+	}
+
+	/**
+	 * Authenticate the remote call using the session identification
+	 * @return
+	 */
+	protected boolean authenticate() {
+		AuthenticatorBean authenticator = (AuthenticatorBean)Component.getInstance("authenticator");
+		authenticator.setSessionId(sessionId);
+		Identity.instance().login();
+
+		if (!Identity.instance().isLoggedIn()) {
+			response.setErrorno(Response.RESP_INVALID_SESSION);
+			return false;
+		}
+
+		FacesMessages.instance().clear();
+		return true;
+	}
+
+	/**
+	 * @return the sessionId
+	 */
+	public String getSessionId() {
+		return sessionId;
+	}
+	
+	
+	/**
+	 * @param data
+	 * @return
+	 */
+	public <E> E deserializeFromXml(String xmldata, Class<E> clazz) {
+		return ObjectSerializer.deserializeFromXml(xmldata, clazz, this);
+	}
+
+
+	/* (non-Javadoc)
+	 * @see org.msh.datastream.XmlDeserializer.ObjectReferenceable#objectByKey(java.lang.Class, java.lang.Object)
+	 */
+	@Override
+	public Object objectByKey(Class clazz, Object key) {
+		if (clazz == TbCase.class) {
+			CaseHome home = (CaseHome)Component.getInstance("caseHome", true);
+			home.setId(key);
+			return home.getInstance();
+		}
+		
+		EntityManager em = (EntityManager)Component.getInstance("entityManager");
+
+		try {
+			return em.find(clazz, key);
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
+
+	/**
+	 * Start a new transaction
+	 */
+	public void beginTransaction() {
+		try {
+			getTransaction().begin();
+			getEntityManager().joinTransaction();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+	
+
+	/**
+	 * Commit a transaction that is under progress 
+	 */
+	public void commitTransaction() {
+		try {
+			if (transaction != null)
+				transaction.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+
+
+	/**
+	 * Roll back a transaction that is under progress 
+	 */
+	public void rollbackTransaction() {
+		try {
+			if (transaction != null)
+				transaction.rollback();
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+	
+
+	/**
+	 * Return the transaction in use by the task
+	 * @return
+	 */
+	protected UserTransaction getTransaction() {
+		if (transaction == null)
+			transaction = (UserTransaction)Component.getInstance("org.jboss.seam.transaction.transaction");
+		return transaction;
+	}
+
+	/**
+	 * Return the {@link EntityManager} instance in use
+	 * @return
+	 */
+	public EntityManager getEntityManager() {
+		return (EntityManager)Component.getInstance("entityManager");
+	}
+
+	/**
+	 * @return the transactional
+	 */
+	public boolean isTransactional() {
+		return transactional;
+	}
+
+	/**
+	 * @param transactional the transactional to set
+	 */
+	public void setTransactional(boolean transactional) {
+		this.transactional = transactional;
+	}
+
+}
