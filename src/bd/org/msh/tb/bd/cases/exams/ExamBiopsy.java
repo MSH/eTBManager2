c9ef47af40b6b2a58613dd606b4bc0b95313ef95
vani <vani@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2012-12-17 20:49:45 +0000

    Bangladesh [Bug, #259] - Exam Biopsy 'save' breaking
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@2075 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

--- src/bd/org/msh/tb/bd/cases/exams/ExamBiopsy.java
+++ src/bd/org/msh/tb/bd/cases/exams/ExamBiopsy.java
@@ -7,6 +7,7 @@ import javax.persistence.Table;
 
 import org.msh.tb.bd.entities.enums.BiopsyResult;
 import org.msh.tb.entities.LaboratoryExamResult;
+import org.msh.tb.entities.enums.SampleType;
 
 @Entity
 @Table(name="exambiopsy")
@@ -18,6 +19,8 @@ public class ExamBiopsy extends LaboratoryExamResult implements Serializable {
 	private static final long serialVersionUID = -1704814081123568208L;
 
 	private BiopsyResult result;
+	
+	private SampleType sampleType;
 
 	public BiopsyResult getResult() {
 		return result;
@@ -25,6 +28,14 @@ public class ExamBiopsy extends LaboratoryExamResult implements Serializable {
 
 	public void setResult(BiopsyResult result) {
 		this.result = result;
+	}
+
+	public SampleType getSampleType() {
+		return sampleType;
+	}
+
+	public void setSampleType(SampleType sampleType) {
+		this.sampleType = sampleType;
 	}
 
 
