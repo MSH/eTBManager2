06d9e36c0e69a25246a59d7ecc2d9387be9d4d3b
vani <vani@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2013-01-15 18:45:40 +0000

    Generic [#250, New Implementation] - ULA Implementation
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@2121 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

---
+++ src/action/org/msh/utils/TextFileReader.java
@@ -0,0 +1,33 @@
+package org.msh.utils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.jboss.seam.annotations.Name;
+
+@Name("textFileReader")
+public class TextFileReader {
+	  
+	public String readFile(String path) throws IOException {
+    	FileInputStream fileStream = (FileInputStream) getClass().getClassLoader().getResourceAsStream("\\WEB-INF\\classes\\org\\msh\\utils\\ula_en.txt");
+    	  try {
+    	    FileChannel fc = fileStream.getChannel();
+    	    MappedByteBuffer bb = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
+    	    return Charset.defaultCharset().decode(bb).toString();
+    	  }
+    	  finally {
+    		  fileStream.close();
+    	  }
+    	}
+}
