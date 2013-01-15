1e328a50dc51a994c03a4499129e925344e65de6
vani <vani@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2013-01-15 18:53:13 +0000

    Generic [#250, New Implementation] - ULA Implementation
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@2125 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

---
+++ database/scripts/script_2013_01_15.sql
@@ -0,0 +1,9 @@
+/************************************************
+ *  Generic + All countries
+ * Jan-15-2013
+ * End User License Implementation
+*************************************************/
+
+update sys_user SET ulaAccepted = 0;
+
+update workspace SET ulaActive = 0;
