5ff4ef36bce7c9a3c4ab82fffe23c1596665f699
vani <vani@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2013-02-19 22:02:59 +0000

    Bangladesh[#Bug, TBForm10-Block7] - Change in logic to calculate the report
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@2196 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

--- src/bd/org/msh/tb/bd/TBForm10Block7.java
+++ src/bd/org/msh/tb/bd/TBForm10Block7.java
@@ -9,9 +9,12 @@ import javax.persistence.EntityManager;
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.international.Messages;
+import org.msh.tb.entities.ExamHIV;
+import org.msh.tb.entities.ExamMicroscopy;
 import org.msh.tb.entities.TbCase;
 import org.msh.tb.entities.enums.Gender;
 import org.msh.tb.entities.enums.HIVResult;
+import org.msh.tb.entities.enums.InfectionSite;
 import org.msh.tb.entities.enums.PatientType;
 import org.msh.tb.indicators.core.Indicator2D;
 import org.msh.tb.na.entities.CaseDispensingNA;
@@ -24,21 +27,23 @@ public class TBForm10Block7 extends Indicator2D{
 	 */
 	private static final long serialVersionUID = -2014368874909253845L;
 	@In(create=true) EntityManager entityManager;
-	private boolean flag = false;
+	private int flag = 0;
 
 	@Override
 	protected void createIndicators() {
 		// TODO Auto-generated method stub
 
 		List<Object[]> lst = new ArrayList<Object[]>();
+		flag = 1;
+		String cond = "exists(select a.id from ExamHIV a where a.tbcase.id = c.id) and exists(select m.id from ExamMicroscopy m where m.tbcase.id = c.id)";
+		setCondition(cond);
 		lst = createQuery().getResultList();
-		
+		flag=0;
 		
 		float cntNewHIVTestedSmearPosM = 0, cntNewHIVTestedSmearPosF = 0, cntNewHIVTestedSmearNegM = 0, cntNewHIVTestedSmearNegF = 0;
 		float cntNewHIVTestedEPM = 0, cntNewHIVTestedEPF = 0, cntNewHIVPosEPM = 0,cntNewHIVPosEPF = 0;
 		float cntReHIVTestedM = 0, cntReHIVTestedF = 0, cntReHIVPosM = 0, cntReHIVPosF = 0;
 		float cntNewHIVPosSmearPosM = 0, cntNewHIVPosSmearPosF = 0, cntNewHIVPosSmearNegM = 0, cntNewHIVPosSmearNegF = 0;
-		float cntReHIVPosSmearPosM = 0, cntReHIVPosSmearPosF = 0, cntReHIVPosSmearNegM = 0, cntReHIVPosSmearNegF = 0;
 		float cntOtherHIVTestedM = 0, cntOtherHIVTestedF = 0, cntOtherHIVPosM = 0, cntOtherHIVPosF = 0;
 		
 		for(Object[] val:lst){
@@ -46,87 +51,85 @@ public class TBForm10Block7 extends Indicator2D{
 			TbCase tbcase = new TbCase();
 			tbcase = entityManager.find(TbCase.class, val[0]);
 			
-				if(tbcase.getPatientType()==PatientType.NEW && isSmearPos(tbcase.getPulmonaryType().getShortName().toString())) {
-					if(tbcase.getPatient().getGender()==Gender.MALE)
-						cntNewHIVTestedSmearPosM++;
+			int tbcaseid = (Integer) val[0];
 			
-					if(tbcase.getPatient().getGender()==Gender.FEMALE)
-						cntNewHIVTestedSmearPosF++;
-					}
+			String hqlMicroscopy = "from ExamMicroscopy m where m.dateRelease in (select max(m.dateRelease) from ExamMicroscopy where m.tbcase.id = " +tbcaseid + ")" + "and m.tbcase.id = " +tbcaseid;
+			ArrayList<ExamMicroscopy> examMicroscopy = (ArrayList<ExamMicroscopy>) entityManager.createQuery(hqlMicroscopy).getResultList();
 			
-				if(tbcase.getPatientType()== PatientType.NEW && isSmearNeg(tbcase.getPulmonaryType().getShortName().toString())) {
-					if(tbcase.getPatient().getGender()==Gender.MALE)
-						cntNewHIVTestedSmearNegM++;
-	
-					if(tbcase.getPatient().getGender()==Gender.FEMALE)
-						cntNewHIVTestedSmearNegF++;
-					}
-				
-				if(tbcase.getPatientType()== PatientType.NEW && (tbcase.getExtrapulmonaryType()!=null) || tbcase.getExtrapulmonaryType2()!=null) {
-					if(tbcase.getPatient().getGender()==Gender.MALE)
-						cntNewHIVTestedEPM++;
-	
-					if(tbcase.getPatient().getGender()==Gender.FEMALE)
-						cntNewHIVTestedEPF++;
-					}
+			String hqlHIV = "from ExamHIV h where h.date in (select max(h.date) from ExamHIV where h.tbcase.id = " +tbcaseid + ")" + "and h.tbcase.id = " +tbcaseid;
+			ArrayList<ExamHIV> examHIV = (ArrayList<ExamHIV>) entityManager.createQuery(hqlHIV).getResultList();
 			
-				if(tbcase.getPatientType()!=PatientType.NEW ){
-					if(tbcase.getPatient().getGender()==Gender.MALE)
-						cntReHIVTestedM++;
-
-					if(tbcase.getPatient().getGender()==Gender.FEMALE)
-						cntReHIVTestedF++;
+			if(!examMicroscopy.isEmpty() && !examHIV.isEmpty() && tbcase.getPatientType()==PatientType.NEW) {
+				if(examMicroscopy.get(0).getResult().isPositive() && tbcase.getPatient().getGender()==Gender.MALE){
+					cntNewHIVTestedSmearPosM++;
+					if(examHIV.get(0).getResult() == HIVResult.POSITIVE)
+						cntNewHIVPosSmearPosM++;
 					}
-				
-				if(isSmearOther(tbcase.getPulmonaryType().getShortName().toString())) {
-					if(tbcase.getPatient().getGender()==Gender.MALE)
-						cntOtherHIVTestedM++;
-
-					if(tbcase.getPatient().getGender()==Gender.FEMALE)
-						cntOtherHIVTestedF++;
-					}		
-				
-			if(val[1]==HIVResult.POSITIVE) {
-					if(tbcase.getPatientType()==PatientType.NEW && isSmearPos(tbcase.getPulmonaryType().getShortName().toString())) {
-						if(tbcase.getPatient().getGender()==Gender.MALE)
-							cntNewHIVPosSmearPosM++;
-						
-						if(tbcase.getPatient().getGender()==Gender.FEMALE)
-							cntNewHIVPosSmearPosF++;	
-						}
-				
-					if(tbcase.getPatientType()==PatientType.NEW && isSmearNeg(tbcase.getPulmonaryType().getShortName().toString())) {
-						if(tbcase.getPatient().getGender()==Gender.MALE)
-							cntNewHIVPosSmearNegM++;
-				
-						if(tbcase.getPatient().getGender()==Gender.FEMALE)
-							cntNewHIVPosSmearNegF++;
-						}
-					
-					if(tbcase.getPatientType()== PatientType.NEW && (tbcase.getExtrapulmonaryType()!=null) || tbcase.getExtrapulmonaryType2()!=null) {
-						if(tbcase.getPatient().getGender()==Gender.MALE)
-							cntNewHIVPosEPM++;
+				if(examMicroscopy.get(0).getResult().isNegative() && tbcase.getPatient().getGender()==Gender.MALE){
+					cntNewHIVTestedSmearNegM++;
+					if(examHIV.get(0).getResult() == HIVResult.POSITIVE)
+						cntNewHIVPosSmearNegM++;
+					}
+				if(examMicroscopy.get(0).getResult().isPositive() && tbcase.getPatient().getGender()==Gender.FEMALE){
+					cntNewHIVTestedSmearPosF++;
+					if(examHIV.get(0).getResult() == HIVResult.POSITIVE)
+						cntNewHIVPosSmearPosF++;
+					}
+				if(examMicroscopy.get(0).getResult().isNegative() && tbcase.getPatient().getGender()==Gender.FEMALE){
+					cntNewHIVTestedSmearNegF++;
+					if(examHIV.get(0).getResult() == HIVResult.POSITIVE)
+						cntNewHIVPosSmearNegF++;
+					}
+			  	}
+		    }
 		
-						if(tbcase.getPatient().getGender()==Gender.FEMALE)
-							cntNewHIVPosEPF++;
-						}
-				
-					if(tbcase.getPatientType()!=PatientType.NEW ){
-						if(tbcase.getPatient().getGender()==Gender.MALE)
-							cntReHIVPosM++;
+		flag = 2;
+		String cond2 = " c.id = e.tbcase.id group by c.id ";
+		setCondition(cond2);
+		setOrderByFields("e.tbcase.id, e.resultDate");
+		List<Object[]> lst2 = createQuery().getResultList();
+		flag = 0;
 		
-						if(tbcase.getPatient().getGender()==Gender.FEMALE)
-							cntReHIVPosF++;
-						}
-					
-					if(isSmearOther(tbcase.getPulmonaryType().getShortName().toString())) {
-						if(tbcase.getPatient().getGender()==Gender.MALE)
-							cntOtherHIVPosM++;
-
-						if(tbcase.getPatient().getGender()==Gender.FEMALE)
-							cntOtherHIVPosF++;
-						}	
+		for(Object[] obj:lst2) {
+			TbCase tbcase = new TbCase();
+			tbcase = entityManager.find(TbCase.class, obj[0]);
+			if(tbcase.getPatientType()==PatientType.AFTER_DEFAULT||tbcase.getPatientType()==PatientType.FAILURE||tbcase.getPatientType()==PatientType.FAILURE_FT||tbcase.getPatientType()==PatientType.FAILURE_RT){
+				if(tbcase.getPatient().getGender()==Gender.MALE){
+					cntReHIVTestedM++;
+					if((HIVResult)obj[2]==HIVResult.POSITIVE)
+						cntReHIVPosM++;
 				}
+				if(tbcase.getPatient().getGender()==Gender.FEMALE)
+					cntReHIVTestedF++;
+					if((HIVResult)obj[2]==HIVResult.POSITIVE)
+						cntReHIVPosF++;		
+			}
+			
+			if(tbcase.getInfectionSite()==InfectionSite.EXTRAPULMONARY){
+				if(tbcase.getPatient().getGender()==Gender.MALE){
+					cntNewHIVTestedEPM++;
+					if((HIVResult)obj[2]==HIVResult.POSITIVE)
+						cntNewHIVPosEPM++;
+				}
+				if(tbcase.getPatient().getGender()==Gender.FEMALE){
+					cntNewHIVTestedEPF++;
+					if((HIVResult)obj[2]==HIVResult.POSITIVE)
+						cntNewHIVPosEPF++;
+				}				
+			}
+			
+			if(tbcase.getPatientType()==PatientType.OTHER){
+				if(tbcase.getPatient().getGender()==Gender.MALE){
+					cntOtherHIVTestedM++;
+					if((HIVResult)obj[2]==HIVResult.POSITIVE)
+						cntOtherHIVPosM++;
+				}
+				if(tbcase.getPatient().getGender()==Gender.FEMALE){
+					cntOtherHIVTestedF++;
+					if((HIVResult)obj[2]==HIVResult.POSITIVE)
+						cntOtherHIVPosF++;
+				}
+			}
 		}
 		
 		Map<String, String> messages = Messages.instance();
@@ -159,39 +162,24 @@ public class TBForm10Block7 extends Indicator2D{
 	
 	@Override
 	public String getHQLSelect() {
-		//return "select e.result, c.patient.gender, c.patientType, c.pulmonaryType.shortName, c.extrapulmonaryType.shortName, c.extrapulmonaryType2.shortName";
-		return " select c.id, e.result ";
+		String sqlsel = null;
+		if(flag == 1)
+			sqlsel = "select c.id, c.patient.gender";
+		if(flag == 2)
+			sqlsel = "select c.id, c.patient.gender, e.result";
+		return sqlsel;
 	}
 	
 	@Override
 	protected String getHQLFrom() {
 		// TODO Auto-generated method stub
-		return "from ExamHIV e inner join e.tbcase c ";
-		
+		String hqlfrom = null;
+		 if(flag==1)
+			 hqlfrom = super.getHQLFrom();
+		 if(flag==2)
+		hqlfrom = "from ExamHIV e inner join e.tbcase c ";
+		 return hqlfrom;
 	}
 	
-	public boolean isSmearPos(String strSmear){
-		flag = false;
-		if(strSmear.equalsIgnoreCase("Smear(+)")){
-			flag = true;
-		}
-		return flag;		
-	}
-	
-	public boolean isSmearNeg(String strSmear){
-		flag = false;
-		if(strSmear.equalsIgnoreCase("Smear(-)")){
-			flag = true;
-		}
-		return flag;		
-	}
-	
-	public boolean isSmearOther(String strSmear){
-		flag = false;
-		if(strSmear.equalsIgnoreCase("O")){
-			flag = true;
-		}
-		return flag;		
-	}
 
 }
