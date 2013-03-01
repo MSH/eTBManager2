9a714970244fa2a064aa85508bd70731cceb0acf
okurasov <okurasov@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2013-03-01 15:19:12 +0000

    AZ Improvement. Add new field to case data
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@2243 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

--- src/az/org/msh/tb/az/entities/enums/CaseFindingStrategy.java
+++ src/az/org/msh/tb/az/entities/enums/CaseFindingStrategy.java
@@ -3,7 +3,8 @@ package org.msh.tb.az.entities.enums;
 public enum CaseFindingStrategy {
 
 	ACTIVE,
-	PASSIVE;
+	PASSIVE,
+	NA;
 	
 	public String getKey() {
 		return "az_AZ." + getClass().getSimpleName() + "." + toString();
