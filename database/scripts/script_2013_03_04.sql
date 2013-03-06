09c78a4e4a6f9cda334f0ed87362396978fc87f4
okurasov <okurasov@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2013-03-06 14:53:18 +0000

    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@2256 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

--- database/scripts/script_2013_03_04.sql
+++ database/scripts/script_2013_03_04.sql
@@ -12,13 +12,13 @@ INSERT INTO `etbmanager`.`userrole` (`id`, `changeable`, `code`, `Role_Name`, `i
 
 INSERT INTO userpermission(canchange, canexecute, grantPermission, profile_id, role_id, caseClassification)
 SELECT false, true, true, id, 197, 0 from userprofile
-WHERE workspace_id = 8
+WHERE workspace_id = 8;
 
 /* and for DR-TB */
 INSERT INTO userpermission(canchange, canexecute, grantPermission, profile_id, role_id, caseClassification)
 SELECT false, true, true, id, 196, 1 from userprofile
-WHERE workspace_id = 8
+WHERE workspace_id = 8;
 
 INSERT INTO userpermission(canchange, canexecute, grantPermission, profile_id, role_id, caseClassification)
 SELECT false, true, true, id, 197, 1 from userprofile
-WHERE workspace_id = 8
+WHERE workspace_id = 8;
