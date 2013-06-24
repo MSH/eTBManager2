11e38d8aec887b00d31aee9ac7f00b94851750d5
okurasov <okurasov@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2013-06-24 12:04:52 +0000

    AZ Improvement. Set diagnosis date true as default in management filters
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@2630 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

--- src/az/org/msh/tb/az/IndicatorControllerAZ.java
+++ src/az/org/msh/tb/az/IndicatorControllerAZ.java
@@ -2,7 +2,9 @@ package org.msh.tb.az;
 
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
+import org.msh.tb.application.App;
 import org.msh.tb.indicators.core.IndicatorController;
+import org.msh.tb.indicators.core.IndicatorFilters;
 
 @Name("indicatorControllerAZ")
 public class IndicatorControllerAZ {
@@ -27,5 +29,14 @@ public class IndicatorControllerAZ {
 	 */
 	public void setExecuting(boolean executing) {
 		indicatorController.setExecuting(executing);
+	}
+	/**
+	 * Set only useDiagnosisDate to true as default in filters for reports
+	 */
+	public void setUseDiagDateDefaultTrue(){
+		IndicatorFilters filters = (IndicatorFilters)App.getComponent("indicatorFilters");
+		filters.setUseIniTreatmentDate(false);
+		filters.setUseRegistrationDate(false);
+		filters.setUseDiagnosisDate(true);
 	}
 }
