6df9e71956da73bde5195112fca914ebd4adeb47
rmemoria <rmemoria@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2013-07-09 13:21:54 +0000

    * Generic [Support] Cases - Exams - Month of treatment being displayed in ordinal numbers in English language (#666)
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@2699 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

--- src/action/org/msh/tb/workspaces/customizable/ExamControl.java
+++ src/action/org/msh/tb/workspaces/customizable/ExamControl.java
@@ -1,7 +1,10 @@
 package org.msh.tb.workspaces.customizable;
 
+import java.text.MessageFormat;
 import java.util.Date;
 
+import org.jboss.seam.core.Locale;
+import org.jboss.seam.international.Messages;
 import org.msh.tb.entities.TbCase;
 
 public class ExamControl {
@@ -16,13 +19,33 @@ public class ExamControl {
 		Integer num = tbcase.getMonthTreatment(dt);
 		
 		if (num > 0) {
-			return "global.monthth";  //Integer.toString(num);
+			String s = Integer.toString(num);
+
+			if ("en".equals(Locale.instance().getLanguage())) {
+				switch (num >= 11 && num <= 13? 0: num % 10) {
+				case 1:
+					s += "st";
+					break;
+				case 2:
+					s += "nd";
+					break;
+				case 3:
+					s += "rd";
+					break;
+
+				default:
+					s += "th";
+					break;
+				}
+			}
+
+			return MessageFormat.format( Messages.instance().get("global.monthth"), s);
 		}
 		
 		Date dtReg = tbcase.getDiagnosisDate();
 		
 		if ((dtReg == null) || (!dt.before(dtReg)))
-			return "cases.exams.zero";
-		else return "cases.exams.prevdt";
+			return Messages.instance().get("cases.exams.zero");
+		else return Messages.instance().get("cases.exams.prevdt");
 	}
 }
