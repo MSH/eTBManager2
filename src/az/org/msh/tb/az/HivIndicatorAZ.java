f1f72eee42e9c99386c4c5f2fdfe73f6dd22f125
okurasov <okurasov@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2013-07-15 14:46:08 +0000

    AZ Improvement. Correct generating of hiv-indicator
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@2727 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

---
+++ src/az/org/msh/tb/az/HivIndicatorAZ.java
@@ -0,0 +1,21 @@
+package org.msh.tb.az;
+
+import org.jboss.seam.annotations.Name;
+import org.msh.tb.indicators.HivIndicator;
+
+/**
+ * Generates case outcome indicator report
+ * @author Utkarsh Srivastava
+ *
+ */
+@Name("hivIndicatorAZ")
+public class HivIndicatorAZ extends HivIndicator {
+	private static final long serialVersionUID = 7098843524696236139L;
+
+	@Override
+	protected String getHQLWhere() {
+		String hql = super.getHQLWhere();
+		hql += " and hiv.date = (select max(ex.date) from ExamHIV ex where ex.tbcase.id = c.id)";
+		return hql;
+	}
+}
