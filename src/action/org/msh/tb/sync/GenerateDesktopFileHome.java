5e0f4c1ce720b9e5d38596fe53ddf001c6bd94ec
rmemoria <rmemoria@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2013-08-29 01:54:12 +0000

    * Generic [Support] Desktop pkg file - Implementing a progress bar when downloading file;
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@2879 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

--- src/action/org/msh/tb/sync/GenerateDesktopFileHome.java
+++ src/action/org/msh/tb/sync/GenerateDesktopFileHome.java
@@ -3,6 +3,11 @@
  */
 package org.msh.tb.sync;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import javax.faces.context.FacesContext;
@@ -24,6 +29,7 @@ import org.msh.tb.entities.Tbunit;
 public class GenerateDesktopFileHome {
 
 	private Integer unitId;
+	private static final int BUFFER_SIZE = 65535;
 	
 	@In EntityManager entityManager;
 	
@@ -41,6 +47,16 @@ public class GenerateDesktopFileHome {
 		Tbunit unit = entityManager.find(Tbunit.class, unitId);
 		if (unit == null)
 			throw new IllegalAccessError("Unit id not valid: " + unitId);
+
+		File file = File.createTempFile("etbm", ".pkg");
+		BufferedOutputStream fout = new BufferedOutputStream( new FileOutputStream(file) );
+		try {
+			// generate the file
+			DesktopIniGeneratorService srv = (DesktopIniGeneratorService)Component.getInstance("desktopIniGeneratorService");
+			srv.generateFile(unit, fout);
+		} finally {
+			fout.close();
+		}
 		
 		// create file name for the unit
 		String filename = unit.getName().getName1();
@@ -49,19 +65,46 @@ public class GenerateDesktopFileHome {
     		filename.substring(0, 9);
     	filename += ".etbm.pkg";
 
+		sendFile(file, filename);
+		
+		// generate the file
+//		DesktopIniGeneratorService srv = (DesktopIniGeneratorService)Component.getInstance("desktopIniGeneratorService");
+//		srv.generateFile(unit, response.getOutputStream());
+		
+	}
+
+	
+	/**
+	 * Send the file to the output response
+	 * @param file
+	 * @param filename
+	 * @throws IOException
+	 */
+	private void sendFile(File file, String filename) throws IOException {
     	// initialize the servlet response
 		FacesContext fc = FacesContext.getCurrentInstance();
 		HttpServletResponse response = (HttpServletResponse) fc.getExternalContext().getResponse();
 		response.setContentType("application/octet-stream");
+		response.setHeader("Content-Length", String.valueOf(file.length()));  
 		response.setHeader("Content-Disposition", "attachment;filename=\"" + filename + "\"");  
 
-		// generate the file
-		DesktopIniGeneratorService srv = (DesktopIniGeneratorService)Component.getInstance("desktopIniGeneratorService");
-		srv.generateFile(unit, response.getOutputStream());
-		
+		BufferedInputStream input = null;
+		BufferedOutputStream output = null;
+		try {
+			input = new BufferedInputStream(new FileInputStream(file), BUFFER_SIZE);
+			output = new BufferedOutputStream(response.getOutputStream(), BUFFER_SIZE);
+            byte[] buffer = new byte[BUFFER_SIZE];  
+            int length;  
+            while ((length = input.read(buffer)) > 0) {  
+                    output.write(buffer, 0, length);
+            }
+        } finally {
+			input.close();
+			output.close();
+		}
 		fc.responseComplete();
 	}
-
+	
 	/**
 	 * @return the unitId
 	 */
