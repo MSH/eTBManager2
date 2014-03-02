413b052ea89cabd754866a6374b4791a3c495399
msantos <msantos@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2014-03-02 17:33:59 +0000

    Bangladesh:
    Execute tasks at 3 am, not 8:17 pm
    
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@3317 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

--- src/action/org/msh/tb/taskscheduling/ScheduleController.java
+++ src/action/org/msh/tb/taskscheduling/ScheduleController.java
@@ -20,7 +20,7 @@ public class ScheduleController implements Serializable {
  
     private static final long serialVersionUID = 7609983147081676186L;
 
-    private static String CRON_INTERVAL_1 = "0 17 20 * * ?"; //every day at 3 am
+    private static String CRON_INTERVAL_1 = "0 0 3 * * ?"; //every day at 3 am
     
     @In
     TagsUpdaterTask tagsUpdaterTask;
