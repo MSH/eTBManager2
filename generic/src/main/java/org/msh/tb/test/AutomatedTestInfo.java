24696b10db9ed8e2cc8afa859fce9438b94ef13c
rmemoria <rmemoria@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2014-09-04 11:53:34 +0000

    * Generic [Feature] New resources for automated tests
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@3694 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

---
+++ generic/src/main/java/org/msh/tb/test/AutomatedTestInfo.java
@@ -0,0 +1,86 @@
+package org.msh.tb.test;
+
+import org.jboss.seam.annotations.Create;
+import org.jboss.seam.annotations.Name;
+import org.msh.tb.application.App;
+import org.msh.tb.entities.UserWorkspace;
+import org.msh.tb.login.SessionData;
+import org.msh.tb.login.UserSession;
+
+import javax.persistence.EntityManager;
+
+/**
+ * Created by ricardo on 04/09/14.
+ */
+@Name("automatedTestInfo")
+public class AutomatedTestInfo {
+    private Integer caseId;
+    private Integer userId;
+    private Integer labId;
+    private Integer unitId;
+    private Integer adminUnitId;
+
+
+    @Create
+    public void initialize() {
+        UserWorkspace uw = UserSession.getUserWorkspace();
+
+        userId = uw.getUser().getId();
+        unitId = uw.getTbunit().getId();
+        labId = uw.getLaboratory() != null? uw.getLaboratory().getId(): null;
+        adminUnitId = uw.getTbunit().getAdminUnit().getId();
+
+        EntityManager em = App.getEntityManager();
+
+        // search for the case
+        // first, search for a case with culture and microscopy exam
+        Number id = (Number)em.createQuery("select max(a.id) from TbCase a where exists (select id from ExamCulture where tbcase.id = a.id)" +
+                " and exists(select id from ExamMicroscopy where tbcase.id = a.id)" +
+                "and a.patient.workspace.id = :wsid")
+                .setParameter("wsid", uw.getWorkspace().getId())
+                .getSingleResult();
+
+        // case was found ?
+        if (id == null) {
+            // so search for any case
+            id = (Number)em.createQuery("select max(a.id) from TbCase a where a.patient.workspace.id = :wsid")
+                    .setParameter("wsid", uw.getWorkspace().getId())
+                    .getSingleResult();
+        }
+
+        caseId = id != null? id.intValue(): null;
+    }
+
+
+    public Integer getCaseId() {
+        return caseId;
+    }
+
+    public void setCaseId(Integer caseId) {
+        this.caseId = caseId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getUnitId() {
+        return unitId;
+    }
+
+    public void setUnitId(Integer unitId) {
+        this.unitId = unitId;
+    }
+
+    public Integer getAdminUnitId() {
+        return adminUnitId;
+    }
+
+    public void setAdminUnitId(Integer adminUnitId) {
+        this.adminUnitId = adminUnitId;
+    }
+}
