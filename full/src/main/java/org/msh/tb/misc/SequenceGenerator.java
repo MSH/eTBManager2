8966b5de5ed94987f292fe3b11e034638d8b8a81
rmemoria <rmemoria@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2015-05-08 14:31:04 +0000

    * Generic [Support] Synchronizing access to validation number generation;
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@3996 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

--- full/src/main/java/org/msh/tb/misc/SequenceGenerator.java
+++ full/src/main/java/org/msh/tb/misc/SequenceGenerator.java
@@ -2,6 +2,7 @@ package org.msh.tb.misc;
 
 import org.jboss.seam.annotations.In;
 import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Synchronized;
 import org.jboss.seam.annotations.Transactional;
 import org.msh.tb.entities.SequenceInfo;
 import org.msh.tb.entities.Workspace;
@@ -9,6 +10,7 @@ import org.msh.tb.entities.Workspace;
 import javax.persistence.EntityManager;
 
 @Name("sequenceGenerator")
+@Synchronized
 public class SequenceGenerator {
 
 	@In(create=true) EntityManager entityManager;
@@ -16,7 +18,7 @@ public class SequenceGenerator {
 
 	/**
 	 * Generates a new sequence number according to the ID
-	 * @param id
+	 * @param seq
 	 * @return
 	 */
 	@Transactional
