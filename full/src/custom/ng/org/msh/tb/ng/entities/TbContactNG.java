c2e1cbf41e77d0761a634e31316dfeae2f276b02
rmemoria <rmemoria@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2015-06-18 12:47:29 +0000

    * Nigeria [Feature] New version of Nigeria with many improvements
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@4065 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

---
+++ full/src/custom/ng/org/msh/tb/ng/entities/TbContactNG.java
@@ -0,0 +1,60 @@
+package org.msh.tb.ng.entities;
+
+import org.msh.etbm.commons.transactionlog.mapping.PropertyLog;
+import org.msh.tb.entities.FieldValue;
+import org.msh.tb.entities.TbContact;
+
+import javax.persistence.*;
+
+
+/**
+* @author Vani Rao
+*
+* Records information about Risk Group in TB Contacts
+*/
+@Entity
+@Table(name="tbcontactng")
+public class TbContactNG extends TbContact {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5958658778809036421L;
+	
+	@ManyToOne(fetch=FetchType.LAZY)
+	@JoinColumn(name="RISK_GROUP")
+	@PropertyLog(messageKey="TbField.RISK_GROUP")
+	private FieldValue riskGroup;
+	
+	private String contactLastName;
+	
+	private String contactOtherName;
+	
+	public FieldValue getRiskGroup() {
+		return riskGroup;
+	}
+	
+	public void setRiskGroup(FieldValue riskGroup) {
+		this.riskGroup = riskGroup;
+	}
+
+	public void setContactLastName(String contactLastName) {
+		this.contactLastName = contactLastName;
+	}
+
+	public String getContactLastName() {
+		return contactLastName;
+	}
+
+	public void setContactOtherName(String contactOtherName) {
+		this.contactOtherName = contactOtherName;
+	}
+
+	public String getContactOtherName() {
+		return contactOtherName;
+	}
+
+	
+	
+
+}
