f4a0183200aaa267fb1ebe650f947cf7c16527d0
msantos <msantos@d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc>
2015-07-01 18:06:53 +0000

    Vietnam
    Solve bug
    
    git-svn-id: https://www.etbmanager.org/svn/etbmanager/etbmanager/trunk@4094 d02e0694-cd7f-c14d-9e3c-2ffdef26b8fc

--- full/src/custom/vi/org/msh/tb/vi/CaseEditingHomeVI.java
+++ full/src/custom/vi/org/msh/tb/vi/CaseEditingHomeVI.java
@@ -4,8 +4,10 @@ package org.msh.tb.vi;
 import org.jboss.seam.ScopeType;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Scope;
+import org.msh.etbm.commons.transactionlog.mapping.LogInfo;
 import org.msh.tb.cases.CaseEditingHome;
 import org.msh.tb.entities.Patient;
+import org.msh.tb.vi.entities.TbCaseVI;
 
 
 /**
@@ -15,6 +17,7 @@ import org.msh.tb.entities.Patient;
  */
 @Name("caseEditingHomeVI")
 @Scope(ScopeType.CONVERSATION)
+@LogInfo(roleName="CASE_DATA", entityClass=TbCaseVI.class)
 public class CaseEditingHomeVI extends CaseEditingHome{
 
 	public String initializeNewNotification() {
